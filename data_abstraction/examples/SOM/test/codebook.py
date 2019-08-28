6314dd60f3ca008fab201dc1560c2a545ec356a8
usangbong <49114064+usangbong@users.noreply.github.com>
2019-08-29 00:57:55 +0900

    Update codebook.py

--- data_abstraction/examples/SOM/test/codebook.py
+++ data_abstraction/examples/SOM/test/codebook.py
@@ -5,3 +5,19 @@ from sklearn.decomposition import PCA
 #from sklearn.decomposition import RandomizedPCA# (randomizedpca is deprecated)
 from .decorators import timeit
 
+class InvalidNodeIndexError(Exception):
+    pass
+
+
+class InvalidMapsizeError(Exception):
+    pass
+
+def generate_hex_lattice(n_rows, n_columns):
+    x_coord = []
+    y_coord = []
+    for i in range(n_rows):
+        for j in range(n_columns):
+            x_coord.append(i*1.5)
+            y_coord.append(np.sqrt(2/3)*(2*j+(1+i)%2))
+    coordinates = np.column_stack([x_coord, y_coord])
+    return coordinates
