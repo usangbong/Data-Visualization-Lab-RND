86b46e360dbb8b9a95b6bdec69a0b94f0946a571
usangbong <49114064+usangbong@users.noreply.github.com>
2019-08-29 00:59:23 +0900

    Update neighborhood.py

--- data_abstraction/examples/SOM/test/neighborhood.py
+++ data_abstraction/examples/SOM/test/neighborhood.py
@@ -1,3 +1,15 @@
 import numpy as np
 import inspect
 import sys
+
+class NeighborhoodFactory(object):
+
+    @staticmethod
+    def build(neighborhood_func):
+        for name, obj in inspect.getmembers(sys.modules[__name__]):
+            if inspect.isclass(obj):
+                if hasattr(obj, 'name') and neighborhood_func == obj.name:
+                    return obj()
+        else:
+            raise Exception(
+                "Unsupported neighborhood function '%s'" % neighborhood_func)
