8f59f35bcd742c12c5d6cbc3c6bd90d969c1dde3
ksy0586 <54608134+ksy0586@users.noreply.github.com>
2019-08-28 18:16:28 +0900

    Create sinplot.py

---
+++ examples/sinplot.py
@@ -0,0 +1,9 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+def sinplot(flip=1):
+    x = np.linspace(0, 14, 100)
+    for i in range(1, 7):
+        plt.plot(x, np.sin(x + i * .5) * (7 - i) * flip)
+
+sinplot()
