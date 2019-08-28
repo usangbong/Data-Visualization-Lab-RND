50fea38b62e5a76856dc542fccc7235ca1b4d037
usangbong <49114064+usangbong@users.noreply.github.com>
2019-08-29 01:01:06 +0900

    Update normalization.py

--- data_abstraction/examples/SOM/test/normalization.py
+++ data_abstraction/examples/SOM/test/normalization.py
@@ -1,3 +1,16 @@
 import numpy as np
 import sys
 import inspect
+
+class NormalizerFactory(object):
+
+    @staticmethod
+    def build(type_name):
+        for name, obj in inspect.getmembers(sys.modules[__name__]):
+            if inspect.isclass(obj):
+                if hasattr(obj, 'name') and type_name == obj.name:
+                    return obj()
+        else:
+            raise Exception("Unknown normalization type '%s'" % type_name)
+            
+            
