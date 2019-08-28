6c1372a15cb7acb7b471fe980919c1b854f5c4ad
usangbong <49114064+usangbong@users.noreply.github.com>
2019-08-29 00:57:43 +0900

    Update codebook.py

--- data_abstraction/examples/SOM/test/codebook.py
+++ data_abstraction/examples/SOM/test/codebook.py
@@ -1,1 +1,7 @@
+import numpy as np
+import scipy as sp
+
+from sklearn.decomposition import PCA
+#from sklearn.decomposition import RandomizedPCA# (randomizedpca is deprecated)
+from .decorators import timeit
 
