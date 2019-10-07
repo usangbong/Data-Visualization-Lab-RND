dba50ec482f4ca6d3fd7950fb39bb1569d659fc8
usangbong <49114064+usangbong@users.noreply.github.com>
2019-10-07 10:20:51 +0900

    Create setup.py

---
+++ data_abstraction/examples/miniSOM/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MiniSom',
+  version= '2.2.1',
+  description='Minimalistic implementation of the Self Organizing Maps (SOM)',
+  author='Giuseppe Vettigli',
+  package_data={'': ['Readme.md']},
+  include_package_data=True,
+  license="CC BY 3.0",
+  py_modules=['minisom'],
+  requires = ['numpy'],
+  url = 'https://github.com/JustGlowing/minisom',
+  download_url = 'https://github.com/JustGlowing/minisom/archive/master.zip',
+  keywords = ['machine learning', 'neural networks', 'clustering', 'dimentionality reduction']
+ )
