72bbaf83e3774f68a0cfb2957ec143e14ee1abbe
usangbong <49114064+usangbong@users.noreply.github.com>
2019-10-07 10:10:19 +0900

    Update minisom.py

--- data_abstraction/examples/miniSOM/minisom.py
+++ data_abstraction/examples/miniSOM/minisom.py
@@ -63,3 +63,17 @@ def fast_norm(x):
     """
     return sqrt(dot(x, x.T))
   
+def asymptotic_decay(learning_rate, t, max_iter):
+    """Decay function of the learning process.
+    Parameters
+    ----------
+    learning_rate : float
+        current learning rate.
+    t : int
+        current iteration.
+    max_iter : int
+        maximum number of iterations for the training.
+    """
+    return learning_rate / (1+t/(max_iter/2))
+  
+  
