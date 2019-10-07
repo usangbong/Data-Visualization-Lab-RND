24013d1cde31ba594d549b0d83e6eef047b8d93c
usangbong <49114064+usangbong@users.noreply.github.com>
2019-10-07 10:08:47 +0900

    Update minisom.py

--- data_abstraction/examples/miniSOM/minisom.py
+++ data_abstraction/examples/miniSOM/minisom.py
@@ -19,3 +19,19 @@ import os
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
 from numpy.testing import assert_array_equal
 import unittest
+
+"""
+    Minimalistic implementation of the Self Organizing Maps (SOM).
+"""
+
+def _build_iteration_indexes(data_len, num_iterations,
+                             verbose=False, random_order=False):
+    """Returns an iterable with the indexes of the samples
+    to pick at each iteration of the training."""
+    iterations = arange(num_iterations) % data_len
+    if random_order:
+        random.shuffle(iterations)
+    if verbose:
+        return _wrap_index__in_verbose(iterations)
+    else:
+        return iterations
