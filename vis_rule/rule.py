78a2e68a943081f039641d5da198c8eb6195ef29
usangbong <49114064+usangbong@users.noreply.github.com>
2019-11-14 02:21:06 +0900

    Update rule.py

--- vis_rule/rule.py
+++ vis_rule/rule.py
@@ -4,3 +4,15 @@ import numpy as np
 import pandas as pd
 import sys
 import csv
+
+visType = ["heat map", "bar chart", "line chart", "tree map"]
+colTypes = ["<class 'str'>", "<class 'char'>", "<class 'int'>", "<class 'float'>", "<class 'double'>", "<class 'bool'>", "<class 'NoneType'>", "<class 'datetime.datetime'>"]
+
+filename = "./test.csv"
+dataSummary = []
+
+KEYS = []
+KEYS_COUNT = []
+VIS = []
+DIMENSION = "D"
+MEASURE = "M"
