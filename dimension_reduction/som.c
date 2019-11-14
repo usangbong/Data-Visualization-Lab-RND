93c39b2b6f3c7a50bfa9859914d28c9f14e21621
usangbong <49114064+usangbong@users.noreply.github.com>
2019-11-15 00:19:38 +0900

    Update som.c

--- dimension_reduction/som.c
+++ dimension_reduction/som.c
@@ -2,3 +2,9 @@
 SOM.c code from https://github.com/tsotne95/SOM/blob/master/som.c
 (https://github.com/tsotne95/SOM)
 **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
