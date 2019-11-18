8510140847152389ff27373696ff924b4d9d9692
usangbong <49114064+usangbong@users.noreply.github.com>
2019-11-19 08:53:47 +0900

    Update vis.py

--- plotly_visualization/vis.py
+++ plotly_visualization/vis.py
@@ -13,5 +13,42 @@ class VisFunctions :
 		return self.case( _htmlFileName, self.funcName, _d,_m, _dimIdxArr, _meaIdxArr, _vis, _arrColumn, _arrData, _pdDataset)
 
 	def callVisFunctions(self, _htmlFileName, _funcName, _d,_m, _dimIdxArr, _meaIdxArr, _vis, _arrColumn, _arrData, _pdDataset) :
-		
+		if _d == 0 : #d==0, m>=1 ##only for pandas format  #print(G_FILENAME_ARR, _d,_m, _dimIdxArr, _meaIdxArr, _vis)
+			tempArrForFuncCall = []
+			tempArrForFuncCall2 = []
+			for j in range(len(_meaIdxArr)):
+				tempArrForFuncCall.append(_arrColumn[_meaIdxArr[j]])
+				tempArrForFuncCall2.append(_arrData[_meaIdxArr[j]])
+			t = getattr(self, _funcName, lambda:"default")
+			if _m==2 :  
+				if _vis=="scatter" :
+					t(_htmlFileName, tempArrForFuncCall2[0], tempArrForFuncCall2[1]) 
+				else :
+					t(_htmlFileName, _pdDataset, tempArrForFuncCall[0], tempArrForFuncCall[1]) 
+			elif _m==3 :
+				t(_htmlFileName, _pdDataset, tempArrForFuncCall[0], tempArrForFuncCall[1], tempArrForFuncCall[2])
+				
+		elif _m == 0 : #d=>1, m==0 ##only for pandas format ##disabled for this version #print(G_FILENAME_ARR, _d,_m, _dimIdxArr, _meaIdxArr, _vis)
+			for i in range(len(_dimIdxArr)):
+				print(_dimIdxArr[i])
+				
+		elif _d==1 : #d==1 m>=1
+			for i in range(len(_dimIdxArr)):
+				tempArrForFuncCall = []
+				#print(G_FILENAME_ARR, _d,_m, _dimIdxArr, _meaIdxArr, _vis)
+				tempArrForFuncCall.append(_arrData[_dimIdxArr[i]])
+				for j in range(len(_meaIdxArr)):
+					tempArrForFuncCall.append(_arrColumn[_meaIdxArr[j]])
+					tempArrForFuncCall.append(_arrData[_meaIdxArr[j]])
+			t = getattr(self, _funcName, lambda:"default")
+			if _m==1 :
+				t(_htmlFileName, tempArrForFuncCall[0], tempArrForFuncCall[1], tempArrForFuncCall[2])
+			elif _m==2 : 
+				t(_htmlFileName, tempArrForFuncCall[0], tempArrForFuncCall[1], tempArrForFuncCall[2], tempArrForFuncCall[3], tempArrForFuncCall[4])
+			elif _m==3 :
+				t(_htmlFileName, tempArrForFuncCall[0], tempArrForFuncCall[1], tempArrForFuncCall[2], tempArrForFuncCall[3], tempArrForFuncCall[4], tempArrForFuncCall[5], tempArrForFuncCall[6])
+		else : #d>1 m>=1
+			print("!!!!!!!!")
+			
+	
 		
