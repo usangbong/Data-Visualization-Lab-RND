6667c83e9969f4861984423e311a416953debe5b
usangbong <49114064+usangbong@users.noreply.github.com>
2019-11-19 08:53:04 +0900

    Update vis.py

--- plotly_visualization/vis.py
+++ plotly_visualization/vis.py
@@ -6,3 +6,8 @@ import plotly.express as px
 
 G_FILENAME_ARR=[]
 class VisFunctions :
+  def vistypeDetection(self, _htmlFileName, _d,_m, _vis, _dimIdxArr, _meaIdxArr, _arrColumn, _arrData, _pdDataset):
+		self.funcName = "_"+str(_d)+"d"+str(_m)+"m_"+str(_vis)
+		self.case = getattr(self, "callVisFunctions", lambda:"default")
+		#print(self.funcName)
+		return self.case( _htmlFileName, self.funcName, _d,_m, _dimIdxArr, _meaIdxArr, _vis, _arrColumn, _arrData, _pdDataset)
