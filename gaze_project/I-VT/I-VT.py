404920f1b0fe15c9da1c60554b69b8207ca40972
usangbong <49114064+usangbong@users.noreply.github.com>
2019-11-22 16:13:27 +0900

    Create I-VT.py

---
+++ gaze_project/I-VT/I-VT.py
@@ -0,0 +1,14 @@
+#ece k.t.
+from __future__ import division
+import numpy as np
+from collections import Counter
+
+#column indices of attributes, check csv file
+index = 0
+timestamp = 1
+question = 2
+x = 3
+y = 4
+user_id = 5
+group_size = 6
+gaze_cue = 7
