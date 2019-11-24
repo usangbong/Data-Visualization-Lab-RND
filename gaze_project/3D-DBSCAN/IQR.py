8d2dd4d78bbb7c1a6168d8b43d8571a809225bda
usangbong <49114064+usangbong@users.noreply.github.com>
2019-11-25 03:33:49 +0900

    Update IQR.py

--- gaze_project/3D-DBSCAN/IQR.py
+++ gaze_project/3D-DBSCAN/IQR.py
@@ -23,3 +23,12 @@ plt.boxplot(tips['tip'], sym="bo")
 plt.title('Box plot of tip')
 plt.xticks([1], ['tip'])
 plt.show()
+
+# Horizontal Box plot with notched box & red color outliers
+plt.boxplot(tips['tip'], 
+            notch=1, # if 'True' then notched box plot
+            sym='rs', # symbol: red square
+            vert=0 # vertical : if 'False' then horizontal box plot
+           )
+plt.show()
+
