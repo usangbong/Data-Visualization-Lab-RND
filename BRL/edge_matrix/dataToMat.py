44607151ccf72f7555a637d65ad6e2251e2c6aa7
usangbong <49114064+usangbong@users.noreply.github.com>
2020-01-07 17:29:19 +0900

    Update dataToMat.py

--- BRL/edge_matrix/dataToMat.py
+++ BRL/edge_matrix/dataToMat.py
@@ -8,3 +8,13 @@ class DataToMat:
 	
 	def setMatrialNumber(self, _matrial_type_num):
 		self.numberOfMatrialType = _matrial_type_num
+
+	def makeMatrixFrame(self):
+		self.rows = []
+		
+		for i in range(0, self.numberOfNode):
+			self._row = []
+			for j in range(0, self.numberOfNode):
+				self._row.append(0)
+			self.rows.append(self._row)
+		self.matrix = self.rows
