c5d10df18a86f38a4a5bc680b0907396f0630808
usangbong <49114064+usangbong@users.noreply.github.com>
2020-01-07 17:30:31 +0900

    Update run.py

--- BRL/edge_matrix/run.py
+++ BRL/edge_matrix/run.py
@@ -8,3 +8,8 @@ if __name__=='__main__':
 	fileName = "./static/1.input"
 	numberOfMatrialType = 4
 	dataLineLength = [115, 30, 20, 20]
+	
+	_fl = fl.FileLoader()
+	_fl.setFileName(fileName)
+	_fl.setDataParameters(dataLineLength, numberOfMatrialType)
+	data = _fl.getDataFromInputFile()
