f7c03ad555454da816291f9e8cc2dbecde9d5203
usangbong <49114064+usangbong@users.noreply.github.com>
2020-01-07 17:30:41 +0900

    Update run.py

--- BRL/edge_matrix/run.py
+++ BRL/edge_matrix/run.py
@@ -13,3 +13,24 @@ if __name__=='__main__':
 	_fl.setFileName(fileName)
 	_fl.setDataParameters(dataLineLength, numberOfMatrialType)
 	data = _fl.getDataFromInputFile()
+
+	matrix = []
+	matrial_type_1 = dtm.DataToMat()
+	_m = []
+	_m = matrial_type_1.makeMatrix(data[0])
+	matrix.append(_m)
+	
+	matrial_type_2 = dtm.DataToMat()
+	_m = []
+	_m = matrial_type_2.makeMatrix(data[1])
+	matrix.append(_m)
+
+	matrial_type_3 = dtm.DataToMat()
+	_m = []
+	_m = matrial_type_3.makeMatrix(data[2])
+	matrix.append(_m)
+
+	matrial_type_4 = dtm.DataToMat()
+	_m = []
+	_m = matrial_type_4.makeMatrix(data[3])
+	matrix.append(_m)
