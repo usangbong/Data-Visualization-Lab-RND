821684939829dfbbecd0921273c5727fb0c402d9
usangbong <49114064+usangbong@users.noreply.github.com>
2020-01-07 17:30:16 +0900

    Create run.py

---
+++ BRL/edge_matrix/run.py
@@ -0,0 +1,10 @@
+# Sangbong Yoo. usangbong@gmail.com
+# python version 3.5
+import os
+import fileLoader as fl
+import dataToMat as dtm
+
+if __name__=='__main__':
+	fileName = "./static/1.input"
+	numberOfMatrialType = 4
+	dataLineLength = [115, 30, 20, 20]
