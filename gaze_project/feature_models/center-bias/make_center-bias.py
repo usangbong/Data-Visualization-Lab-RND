efa575dae2f95a48438c9af5c8e67818f24a3f75
usangbong <49114064+usangbong@users.noreply.github.com>
2020-03-13 01:06:49 +0900

    Update make_center-bias.py

--- gaze_project/feature_models/center-bias/make_center-bias.py
+++ gaze_project/feature_models/center-bias/make_center-bias.py
@@ -22,3 +22,27 @@ file_list = os.listdir(IMG_DIR)
 OUT_DIR = "./out/"
 create_dir(OUT_DIR)
 
+for IMG_NAME in file_list:
+    IMG_FULL_PATH = IMG_DIR+IMG_NAME
+    i = Image.open(IMG_FULL_PATH)
+    img_width, img_height = i.size
+
+    # center max val = 10, min val = 0
+    center_x = img_width/2.0
+    center_y = img_height/2.0
+    x_mv = 5.0/center_x
+    y_mv = 5.0/center_y
+
+    center_bias_mat = []
+    for _c in range(0, img_width):
+        _row = []
+        for _r in range(0, img_height):
+            _val = x_mv*_c + y_mv*_r
+            _row.append(_val)
+            
+        center_bias_mat.append(_row)
+
+
+    OUT_FULL_PATH = OUT_DIR+IMG_NAME[:-4]+"_center-bias.csv"
+
+    csvWriter(OUT_FULL_PATH, center_bias_mat)
