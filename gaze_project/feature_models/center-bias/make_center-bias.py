2d295d256f597726f4f09412864625f64119a68f
usangbong <49114064+usangbong@users.noreply.github.com>
2020-03-13 01:06:17 +0900

    Update make_center-bias.py

--- gaze_project/feature_models/center-bias/make_center-bias.py
+++ gaze_project/feature_models/center-bias/make_center-bias.py
@@ -1,3 +1,11 @@
 import os
 import csv
 from PIL import Image
+
+def create_dir(_dirpath):
+    try:
+        if not os.path.exists(_dirpath):
+            os.makedirs(_dirpath)
+    except OSError:
+        print("Error: creating directory."+_dirpath)
+        
