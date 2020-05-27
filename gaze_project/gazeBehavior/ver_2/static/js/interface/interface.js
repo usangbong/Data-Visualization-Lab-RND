0704ccdcf5b8629c4f224fadb777294e8b1e98b8
usangbong <49114064+usangbong@users.noreply.github.com>
2020-05-27 23:14:29 +0900

    Add files via upload

---
+++ gaze_project/gazeBehavior/ver_2/static/js/interface/interface.js
@@ -0,0 +1,291 @@
+let temp = [];
+let __t = [];
+let _t = {
+	'x': 412.4542124669535, 'y': 306.7882471890983, 'duration': 47, 'clss': 0
+};
+__t.push(_t);
+_t = {
+	'x': 410.989011, 'y': 311.8541033, 'duration': 1, 'clss': 3
+};
+__t.push(_t);
+_t = {
+	'x': 537.7335649980503, 'y': 301.41225397938206, 'duration': 16, 'clss': 1
+};
+__t.push(_t);
+_t = {
+	'x': 692.3076923, 'y': 284.4984802, 'duration': 1, 'clss': 2
+};
+__t.push(_t);
+_t = {
+	'x': 722.469400633399, 'y': 268.30750977413163, 'duration': 49, 'clss': 0
+};
+__t.push(_t);
+_t = {
+	'x': 740.6593407, 'y': 260.7902736, 'duration': 1, 'clss': 3
+};
+__t.push(_t);
+_t = {
+	'x': 750.7455507393778, 'y': 245.07076847017288, 'duration': 9, 'clss': 1
+};
+__t.push(_t);
+_t = {
+	'x': 756.043956, 'y': 213.3738602, 'duration': 1, 'clss': 2
+};
+__t.push(_t);
+temp.push(__t);
+
+// update data panel
+let iDataColumns = $('#data_columns');
+iDataColumns.append("<br>");
+let dlColumns = $(`
+	<selectgroup>
+        <option value="d_x">x</option>
+        <option value="d_y">y</option>
+    </selectgroup>
+`);
+
+iDataColumns.append(dlColumns);
+
+let iMeasurement = $('#data_measurement');
+iMeasurement.append("<br>");
+let dlMeasurement = $(`
+	<input type='radio' value='m_duration' checked/>
+	<label>duration</label>
+	<input type='radio' value='m_pupil_diameter'>pupil diameter</input>
+`);
+iMeasurement.append(dlMeasurement);
+
+let iEvent = $('#data_event');
+iEvent.append("<br>");
+let dlEvent = $(`
+	<input type='checkbox' id="e_fix" name='e_fix' checked/>
+	<label for="e_fix">fixation</label>
+	<input type='checkbox' id="e_sac" name='e_sac' checked/>
+	<label for="e_sac">saccade</label>
+	<input type='checkbox' id="e_pso" name='e_pso' />
+	<label for="e_pso">PSO</label>
+	<input type='checkbox' id="e_bli" name='e_bli' />
+	<label for="e_bli">blink</label>
+	<input type='checkbox' id="e_unk" name='e_unk' />
+	<label for="e_unk">unknown</label>
+`);
+iEvent.append(dlEvent);
+
+
+
+drawGridHeat_fixDur(temp);
+
+function initInterface(){
+        
+}
+
+function drawGridHeat_fixDur(dataset){
+	console.log("drawGridHeat_fixDur");
+	
+	let dim_1 = [];
+	let dim_2 = [];
+	let _value = [];
+	for(let i=0; i<dataset.length; i++){
+		let _v = [];
+		
+		for(let j=0; j<dataset[i].length; j++){
+			dim_2.push("t"+(j+1).toString());
+			_v.push(dataset[i][j].duration);
+		}
+		dim_1.push("o"+(i+1).toString());
+		_value.push(_v);
+	}
+
+	// make index data (instead *.tsv file)
+	let indexData = [];
+	for(let i=0; i<dim_1.length; i++){
+		if (i==0){
+			indexData.push(["dim1", "dim2"]);
+		}
+		for(let j=0; j<dim_2.length; j++){
+			indexData.push([i+1, j+1]);
+		}
+	}
+
+	// make input data: index & value
+	let inputdata = [];
+	for(let i=1; i<indexData.length; i++){
+		let _row = [indexData[i][0], indexData[i][1], _value[indexData[i][0]-1][i-1]];
+		inputdata.push(_row);
+	}
+	//console.log(inputdata);
+
+	//UI configuration
+	const margin = { top: 50, right: 0, bottom: 100, left: 30 },
+		width = 400 - margin.left - margin.right,
+		height = 430 - margin.top - margin.bottom,
+		gridSize = Math.floor(width / 10),
+		legendElementWidth = gridSize*1,
+		buckets = 9,
+		colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]; // alternatively colorbrewer.YlGnBu[9]
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	const yLabel = svg.selectAll(".yLabel")
+		.data(dim_1)
+		.enter().append("text")
+			.text(function(d){return d;})
+			.attr("x",0)
+			.attr("y", function(d, i){return i*gridSize;})
+		.style("text-anchor", "end")
+		.attr("transform", "translate(-6,"+gridSize/1.5+")");
+
+	const xLabel = svg.selectAll(".xLabel")
+		.data(dim_2)
+		.enter().append("text")
+			.text(function(d){return d;})
+			.attr("x", function(d, i){return i*gridSize;})
+			.attr("y",0)
+		.style("text-anchor", "middle")
+		.attr("transform", "translate(" + gridSize / 2 + ", -6)");
+
+	const heatmapChart = function(_data){
+		//console.log(_data);
+		const colorScale = d3.scaleQuantile()
+            .domain([0, buckets - 1, d3.max(_data, function(d){return d[2];})])
+            .range(colors);
+
+        const cards = svg.selectAll(".card")
+        	.data(_data, function(d){return d[0]+':'+d[1];});
+    	cards.append("title");
+
+    	cards.enter().append("rect")
+    		.attr("x", function(d){return (d[1]-1)*gridSize;})
+    		.attr("y", function(d){return (d[0]-1)*gridSize;})
+    		.attr("rx", 4)
+    		.attr("ry", 4)
+    		.attr("width", gridSize)
+    		.attr("height", gridSize)
+    		.style("fill", colors[0])
+		.merge(cards)
+			.transition()
+			.duration(1000)
+			.style("fill", function(d){return colorScale(d[2]);});
+
+		cards.select("title").text(function(d){return d[2];});
+		cards.exit().remove();
+
+		const legend = svg.selectAll(".legend")
+			.data([0].concat(colorScale.quantiles()), function(d){return d;});
+
+		const legend_g = legend.enter().append("g")
+			.attr("class", "legend");
+
+		legend_g.append("rect")
+			.attr("x", function(d, i){return legendElementWidth*i;})
+			.attr("y", height)
+			.attr("width", legendElementWidth)
+			.attr("height", gridSize/2)
+			.style("fill", function(d, i){return colors[i];});
+
+		legend_g.append("text")
+			.attr("class", "mono")
+			.text(function(d){return "≥ "+Math.round(d);})
+			.attr("x", function(d, i){return legendElementWidth*i;})
+			.attr("y", height + gridSize);
+
+		legend.exit().remove();
+	}
+	heatmapChart(inputdata);
+}
+
+selectAOIgrid([0,0], 10, 10);
+
+function selectAOIgrid(dataset, setRow, setCol){
+	// UI setting: "ADD" & "CLEAR" button
+	let AOIselector = $('#aoi_selector');
+	AOIselector.append("<br>");
+	let AOIGrid = $(`
+		<button onclick="AOI_add_function()">ADD</button>
+		<button onclick="AOI_clear_function()">CLEAR</button>
+	`);
+	AOIselector.append(AOIGrid);
+
+	let stiGrid = [];
+	let xpos = 1;
+	let ypos = 1;
+	let width = 30;
+	let height = 30;
+	let click = 0;
+
+	for(let _r=0; _r<setRow; _r++){
+		let _row = [];
+
+		for(let _c=0; _c<setCol; _c++){
+			let _col = {
+				x: xpos,
+				y: ypos,
+				width: width,
+				height: height,
+				click: click
+			};
+
+			_row.push(_col);
+			xpos += width;
+		}
+		stiGrid.push(_row);
+
+		xpos = 1;
+		ypos += height;
+	}
+
+	let grid = d3.select("body").append("svg")
+		.attr("width", "510px")
+		.attr("height", "510px");
+
+	let row = grid.selectAll(".row")
+		.data(stiGrid)
+		.enter().append("g")
+		.attr("class", "row");
+
+	let column = row.selectAll(".square")
+		.data(function(d){return d;})
+		.enter().append("rect")
+		.attr("class", "square")
+		.attr("x", function(d){return d.x;})
+		.attr("y", function(d){return d.y;})
+		.attr("width", function(d){return d.width;})
+		.attr("height", function(d){return d.height;})
+		.style("fill", "#fff")
+		.style("stroke", "#222")
+		.on("click", function(d){
+			d.click++;
+			if((d.click)%4==0){ d3.select(this).style("fill", "#fff"); }
+			if((d.click)%4==1){ d3.select(this).style("fill", "#2c93e8"); }
+			if((d.click)%4==2){ d3.select(this).style("fill", "#f56c4e"); }
+			if((d.click)%4==3){ d3.select(this).style("fill", "#838690"); }
+		})
+
+}
+
+function AOI_add_function(){
+	alert("add btn");
+}
+
+function AOI_clear_function(){
+	alert("clear btn");
+}
+
+function getRandomValue(_type, _min, _max){
+	let _rVal = 0;
+	if(_type==0){
+		// float
+		_rVal = Math.random()*(_max-_min)+_min;
+	}else if(_type==1){
+		// int
+		_rVal = Math.floor(Math.random()*(_max-_min))+_min;
+	}else{
+		console.log("Select type: float (0) or int (1).");
+	}
+
+	return _rVal;
+}
