ef0020ae5056354ecbe405a623a10be4625cb5e8
vfgtr8746 <vfgtr8746@gmail.com>
2020-06-09 14:21:47 +0900

    Condition Scripts

---
+++ VR/dance_Reinforcement/VR_AI Dance/Assets/Scripts/Condition/RadiusConditionAgent.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RadiusConditionAgent : Agent
+{
+    public GameObject Head;
+
+    CorWrong cw;
+    MoveObject moveObject;
+
+    int select;
+
+    private void Awake()
+    {
+        cw = GameObject.Find("CorWrong").GetComponent<CorWrong>();
+        moveObject = Head.GetComponent<MoveObject>();
+
+        StartCoroutine(timeChecker());
+    }
+
+    public override void CollectObservations()
+    {
+        AddVectorObs(moveObject.getOriginPos());
+        AddVectorObs(moveObject.getPos());
+        AddVectorObs(moveObject.getRadius());
+        AddVectorObs(moveObject.getAverageRadius());
+        AddVectorObs(select);
+    }
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        select = Mathf.FloorToInt(vectorAction[0]);
+
+        float radius = moveObject.getRadius();
+        float averageRadius = moveObject.getAverageRadius();
+
+        if(select == 0)
+        {
+            if(radius >= averageRadius)
+            {
+                AddReward(1f);
+                cw.correct++;
+            }
+
+            else
+            {
+                AddReward(-1f);
+                cw.wrong++;
+            }
+        }
+
+        else if(select == 1)
+        {
+            if(radius >= averageRadius)
+            {
+                AddReward(-1f);
+                cw.wrong++;
+            }
+
+            else
+            {
+                AddReward(1f);
+                cw.correct++;
+            }
+        }
+    }
+
+    IEnumerator timeChecker()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(100f);
+
+            cw.change = true;
+            Done();
+        }
+    }
+}
---
+++ VR/dance_Reinforcement/VR_AI Dance/Assets/Scripts/Condition/RaycastConditionAgent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RaycastConditionAgent : Agent
+{
+    public GameObject objList;
+
+    public int raySel;
+
+    int select;
+
+    CorWrong cw;
+
+    void Awake()
+    {
+        cw = GameObject.Find("CorWrong").GetComponent<CorWrong>();
+
+        StartCoroutine(timeChecker());
+    }
+
+    public override void CollectObservations()
+    {
+        AddVectorObs(select);
+        AddVectorObs(raySel);
+    }
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        select = Mathf.FloorToInt(vectorAction[0]);
+
+        if(raySel == select)
+        {
+            AddReward(1f);
+            cw.correct++;
+        }
+
+        else
+        {
+            AddReward(-0.2f);
+            cw.wrong++;
+        }
+    }
+
+    IEnumerator timeChecker()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(100f);
+
+            cw.change = true;
+            Done();
+        }
+    }
+}
