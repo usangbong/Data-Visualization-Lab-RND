862fc5d6892a25578a2e350fabace2c850254aa5
usangbong <49114064+usangbong@users.noreply.github.com>
2020-07-24 06:40:28 +0900

    Add files via upload

---
+++ gaze_project/gazeBehavior/ver_3/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import Sidebar from './Components/Sidebar';
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <Sidebar></Sidebar>
+    </div>
+  );
+}
+
+export default App;
