f0de7472f89de0059a19d8b80b1c89c42ea7db68
usangbong <49114064+usangbong@users.noreply.github.com>
2020-07-28 02:50:33 +0900

    Add files via upload

--- gaze_project/gazeBehavior/ver_3/src/App.js
+++ gaze_project/gazeBehavior/ver_3/src/App.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Sidebar from './Components/Sidebar';
+//import FileAdd from './Components/FileAdd';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  // const [currentTime, setCurrentTime] = useState(0);
 
-  useEffect(() => {
-    fetch('/api/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
-
+  // useEffect(() => {
+  //   fetch('/api/time').then(res => res.json()).then(data => {
+  //     setCurrentTime(data.time);
+  //   });
+  // }, []);
+  
   return (
     <div className="App">
       <Sidebar></Sidebar>
