ea80ff9f94f1396db99bf32412a7bbfc4e6736d9
Seongbum Seo <sbum.seo@gmail.com>
2020-08-18 03:36:05 +0900

    Delete app.py

--- map/scripts/app.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('data/EMD.json', encoding="utf-8") as json_file:
-  data = json.load(json_file)
-  for feature in data.features:
-    properties = feature.properties
-    geometry = feature.geometry
-    for 
