c13b0c3379c75643eacacdd5192c216ebcf91b58
Seongbum Seo <sbum.seo@gmail.com>
2020-08-18 03:38:17 +0900

    Update voronoi_rebuild.js

--- map/scripts/voronoi_rebuild.js
+++ map/scripts/voronoi_rebuild.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const voronoi = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/kr_village_voronoi_intersection.json')));
+const voronoi = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/kr_village_voronoi_intersection_old.json')));
 
 const result = {
   type: 'FeatureCollection',
   features: []
 };
 for (let i = 0; i < voronoi.features.length; i++) {
-  voronoi.features[i].properties.id = i;
+  voronoi.features[i].properties.emdlid = i;
 }
 
 const output = JSON.stringify(voronoi);
-fs.writeFile(path.resolve(__dirname, '../data/kr_village_voronoi_id.json'), output, 'utf8', (err) => {
+fs.writeFile(path.resolve(__dirname, '../data/kr_village_voronoi_intersection.json'), output, 'utf8', (err) => {
   if (err) throw err;
   console.log('output:', voronoi.features.length);
 });
