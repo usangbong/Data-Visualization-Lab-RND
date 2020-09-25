ebb637b90f518c73184b2395092070bf946424cf
usangbong <49114064+usangbong@users.noreply.github.com>
2020-09-25 12:27:14 +0900

    Add files via upload

---
+++ gaze_project/gazeBehavior/ver_4/templates/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as Data } from './Data';
+export { default as SpatialVariance } from './SpatialVariance';
+export { default as Analysis } from './Analysis';
+export { default as Visualization } from './Visualization';
