a1ac6715b10466c4cf91e532dcfe4ec87fc4e1ec
usangbong <49114064+usangbong@users.noreply.github.com>
2020-09-25 12:28:20 +0900

    Add files via upload

---
+++ gaze_project/gazeBehavior/ver_4/templates/src/pages/SpatialVariance.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class SpatialVariance extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="page-header">
+          <h1>Spatial Variance</h1>
+        </div>
+
+        <div className="page-section">
+          <div style={{margin: '10px 0'}}>
+            <button>A</button>
+          </div>
+          <div style={{margin: '10px 0'}}>
+            <button>B</button>
+          </div>
+          <div style={{margin: '10px 0'}}>
+            <button>C</button>
+          </div>
+          <div style={{margin: '10px 0'}}>
+            <button>D</button>
+          </div>
+          <div style={{margin: '10px 0'}}>
+            <button>E</button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default SpatialVariance;
