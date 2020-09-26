39d70f209e6b6f1d767188d7d1be8b680b670784
usangbong <49114064+usangbong@users.noreply.github.com>
2020-09-26 18:47:18 +0900

    Add files via upload

--- gaze_project/gazeBehavior/ver_4/templates/src/pages/SpatialVariance.js
+++ gaze_project/gazeBehavior/ver_4/templates/src/pages/SpatialVariance.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 class SpatialVariance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spRes: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://${window.location.hostname}:5000/static/output/spatial_variance.json`)
+      .then(response => {
+        this.setState({
+          spRes: 
+            response.data
+        });
+      });
+  }
+
   render() {
+    const { spRes } = this.state;
     return (
       <>
         <div className="page-header">
           <h1>Spatial Variance</h1>
         </div>
+
+    <p>{this.props.spRes}</p>
 
         <div className="page-section">
           <div style={{margin: '10px 0'}}>
