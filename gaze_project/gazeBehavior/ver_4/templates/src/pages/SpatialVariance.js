d2d9e9173a5e45b439c1df931a824cc1226aeec0
usangbong <49114064+usangbong@users.noreply.github.com>
2020-09-29 16:12:21 +0900

    Add files via upload

--- gaze_project/gazeBehavior/ver_4/templates/src/pages/SpatialVariance.js
+++ gaze_project/gazeBehavior/ver_4/templates/src/pages/SpatialVariance.js
@@ -1,35 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+
+import '../../node_modules/ag-grid-enterprise';
+import '../../node_modules/ag-grid-community/dist/styles/ag-grid.css';
+import '../../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 class SpatialVariance extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      spRes: []
+      tableData: [{
+        featureType: '',
+        stimulusClass: '',
+        stimulusName: '',
+        spValue: ''
+      }], 
+      rowData: []
     };
   }
 
   componentDidMount() {
     axios.get(`http://${window.location.hostname}:5000/static/output/spatial_variance.json`)
       .then(response => {
+        var _data = response.data;
+        console.log(_data)
+        var _td = []
+        var _getData = {
+          featureType: response.data[0],
+          stimulusClass: response.data[1],
+          stimulusName: response.data[2],
+          spValue: response.data[3]
+        };
+        _td.push(_getData);
+
         this.setState({
-          spRes: 
-            response.data
+          tableData: 
+            _td
+        });
+        var _t = [];
+        var _vv = {make: "Toyota", model: "Celica", price: 35000};
+        _t.push(_vv);
+        _vv = {make: "Ford", model: "Mondeo", price: 32000};
+        _t.push(_vv);
+        _vv = {make: "Porsche", model: "Boxter", price: 72000};
+        _t.push(_vv);
+        _vv = {make: "Toyota", model: "Celica", price: 35000};
+        _t.push(_vv);
+        this.setState({
+          rowData:
+            _t
         });
       });
   }
 
   render() {
-    const { spRes } = this.state;
+    const {gridApi, setGridApi} = this.state;
+    const {gridColumnApi, setGridColumnApi} = this.state;
+    const {rowData, setRowData} = this.state; 
+      
+    const { tableData } = this.state;
+
+    // function onGridReady(params) {
+    //     setGridApi(params.api);
+    //     setGridColumnApi(params.columnApi);
+    // }
+    
     return (
       <>
         <div className="page-header">
           <h1>Spatial Variance</h1>
         </div>
-
-    <p>{this.props.spRes}</p>
-
+        
         <div className="page-section">
+          <p>{this.state.spRes}</p>
+
           <div style={{margin: '10px 0'}}>
             <button>A</button>
           </div>
@@ -45,7 +90,20 @@ class SpatialVariance extends React.Component {
           <div style={{margin: '10px 0'}}>
             <button>E</button>
           </div>
+
+          <div className="ag-theme-alpine" style={ { height: 400, width: 800 } }>
+            <AgGridReact
+              rowData={tableData}>
+              <AgGridColumn field="featureType"></AgGridColumn>
+              <AgGridColumn field="stimulusClass"></AgGridColumn>
+              <AgGridColumn field="stimulusName"></AgGridColumn>
+              <AgGridColumn field="spValue"></AgGridColumn>
+            </AgGridReact>
+          </div>
+
+          
         </div>
+
       </>
     );
   }
