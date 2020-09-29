5d2dcfb271f7f9adc6e58b07d8f31d04c9476c20
usangbong <49114064+usangbong@users.noreply.github.com>
2020-09-29 16:11:04 +0900

    Add files via upload

--- gaze_project/gazeBehavior/ver_4/app2.py
+++ gaze_project/gazeBehavior/ver_4/app2.py
@@ -1,7 +1,13 @@
-from flask import *
-from flask_cors import CORS
+# from flask import *
+# from flask_cors import CORS
 
-app = Flask(__name__)
-if __name__ == '__main__':  
-  app.run(debug=True, port=5001)
-CORS(app)
+# app = Flask(__name__)
+# if __name__ == '__main__':  
+#   app.run(debug=True, port=5001)
+# CORS(app)
+
+import csv
+import sys
+import os
+import math
+
