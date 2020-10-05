9f0950eec8982fb40a3828d121f493d4425550f4
Soobin <tn12qls@gmail.com>
2020-10-05 17:56:52 +0900

    iatk

---
+++ VRvis/UNITYvisaulization ver1/Assets/MakeMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IATK;
+public class MakeMesh : MonoBehaviour
+{
+    public BigMesh bigMesh;
+    bool isSave = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSave)
+        {
+            bigMesh.SaveBigMesh();
+            isSave = true;
+        }
+    }
+}
