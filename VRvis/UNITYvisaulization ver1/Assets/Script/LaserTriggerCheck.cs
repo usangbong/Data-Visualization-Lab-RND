b66ee075f9608d74133ca7058f1e2a7363209340
Soobin <tn12qls@gmail.com>
2020-10-05 17:59:41 +0900

    unity project

---
+++ VRvis/UNITYvisaulization ver1/Assets/Script/LaserTriggerCheck.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class LaserTriggerCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Object"))
+        {
+            Debug.Log("TriggerCheck!");
+            Debug.Log("Trigger Position : " + other.transform.position);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Object")) {
+            Debug.Log("TriggerStay!" + other.name);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Object"))
+        {
+            Debug.Log("Trigger Out Check");
+        }   
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("On Collision Enter");
+        Debug.Log("Test : " + other.collider.tag);
+       // Vector3 pos_t = other.contacts[0].point;
+        //Debug.Log("Collistion Vector Check ! " + pos_t);
+
+        if (other.collider.CompareTag("Object"))
+        {
+            Vector3 pos = other.contacts[0].point;
+            Debug.Log("Collistion Vector Check ! : " + other.gameObject.name + pos);
+        }
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.collider.CompareTag("Object"))
+        {
+            Vector3 pos = other.contacts[0].point;
+            Debug.Log("Collistion Vector Stay ! : " + other.collider.name + pos);
+        }
+    }
+}
---
+++ VRvis/UNITYvisaulization ver1/Assets/Script/MakeLaser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+
+public class MakeLaser : MonoBehaviour
+{
+    public GameObject laserPrefab, EyeRayObj;
+
+    SRanipal_GazeRaySample_v2 rayTest;
+
+    Vector3 start, last;
+    GameObject laser;
+
+    void Start()
+    {
+        rayTest = EyeRayObj.GetComponent<SRanipal_GazeRaySample_v2>();
+
+        laser = Instantiate(laserPrefab);
+    }
+
+    void Update()
+    {
+        start = rayTest.start;
+        last = rayTest.last;
+
+        laser.transform.position = Vector3.Lerp(start, last, 0.5f);
+        laser.transform.LookAt(last);
+        laser.transform.localScale = new Vector3(laser.transform.localScale.x,
+            laser.transform.localScale.y, Vector3.Distance(start, last));
+    }
+}
