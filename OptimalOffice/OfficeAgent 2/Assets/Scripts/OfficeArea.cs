4c47d8d1d749ce6321cc70874618083571d6a40d
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2020-10-12 13:37:34 +0900

    중복 오브젝트 감점

--- OptimalOffice/OfficeAgent 2/Assets/Scripts/OfficeArea.cs
+++ OptimalOffice/OfficeAgent 2/Assets/Scripts/OfficeArea.cs
@@ -19,23 +19,76 @@ public class OfficeArea : MonoBehaviour
         {
             for(int j=0;j<8;j++)
             {
-                cells[i,j] = new Cell((i+1)*(j+1), -10.5f + j*3f, 6f - i*3f);
+                cells[i,j] = new Cell((i+1)*(j+1) - 1, -10.5f + j*3f, 6f - i*3f);
+            }
+        }
+    }
+
+    public Cell FindCell(int idx)
+    {
+        for (int i=0;i<5;i++)
+        {
+            for(int j=0;j<8;j++)
+            {
+                if (cells[i, j].getIdx() == idx) return cells[i, j];
+            }
+        }
+
+        return null;
+    }
+
+    public void DeductionToAgent()
+    {
+        List<GameObject> agentList;
+
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<8;j++)
+            {
+                if(cells[i,j].getAgentCount() > 1)
+                {
+                    agentList = cells[i, j].getAgentList();
+
+                    for(int k=0;k< agentList.Count;k++)
+                    {
+                        agentList[k].GetComponent<OfficeAgent>().AddReward(-1f * cells[i,j].getAgentCount());
+                    }
+                }
             }
         }
     }
 }
 
-class Cell
+public class Cell
 {
     int idx;
     Vector2 minPos, maxPos;
+    List<GameObject> agentList;
 
+    public int getIdx() { return idx; }
+    public int getAgentCount() { return agentList.Count; }
+    public List<GameObject> getAgentList() { return agentList; }
     public Vector2 getMinPos() { return minPos; }
     public Vector2 getMaxPos() { return maxPos; }
+
+    public Vector3 getObjectPos(GameObject agent)
+    {
+        Vector3 centerPos;
+
+        centerPos.x = (minPos.x + maxPos.x) / 2.0f;
+        centerPos.y = 0f;
+        centerPos.z = (minPos.y + maxPos.y) / 2.0f;
+
+        agentList.Add(agent);
+
+        return centerPos;
+    }
 
     public Cell(int _idx, float c_x, float c_y)
     {
         idx = _idx;
+
+        agentList = new List<GameObject>();
 
         minPos.x = c_x - 1.5f;
         minPos.y = c_y - 1.5f;
