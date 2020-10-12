af96d10926def46db33702ea96ae1fd783780c88
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2020-10-12 16:31:08 +0900

    열거형 추가, 함수이름 변경, 주석 추가, 2차원 -> 3차원 변경

--- OptimalOffice/OfficeAgent 2/Assets/Scripts/OfficeArea.cs
+++ OptimalOffice/OfficeAgent 2/Assets/Scripts/OfficeArea.cs
@@ -4,34 +4,45 @@ using UnityEngine;
 
 public class OfficeArea : MonoBehaviour
 {
+    public enum OverState { NOT_OVER, OVER_RIGHT, OVER_LEFT, OVER_UP, OVER_DOWN };    
+
+    //Cell의 개수 (가로, 세로)
     public int x_Count, z_Count;
-    float x_diff, z_diff;
+    //Cell 크기 (가로, 세로)
+    public static float x_Size, z_Size;
 
     //Delete
     public GameObject tempObj;
 
+    //생성 Cell List
     Cell[,] cells = new Cell[5,8];
 
-    const float minX = -12f;
-    const float maxX = 12f;
-    const float minZ = -7.5f;
-    const float maxZ = 7.5f;
+    //Area의 max, min 좌표
+    public static float minX = -12f;
+    public static float maxX = 12f;
+    public static float minZ = -7.5f;
+    public static float maxZ = 7.5f;
 
+    //Cell x_Count * z_Count개 생성
     private void Start()
     {
-        x_diff = (float)(maxX - minX) / x_Count;
-        z_diff = (float)(maxZ - minZ) / z_Count;
+        //Cell 크기 계산
+        x_Size = (float)(maxX - minX) / x_Count;
+        z_Size = (float)(maxZ - minZ) / z_Count;
 
+        //Cell 생성
         for(int i=0;i<5;i++)
         {
             for(int j=0;j<8;j++)
             {
-                cells[i, j] = new Cell((i + 1) * (j + 1) - 1, (minX + x_diff / 2) + (j * x_diff), (maxZ - z_diff / 2) - (i * z_diff), Instantiate(tempObj)); //tempObj delete
+                //index (0~39), x, z의 가운데 좌표
+                cells[i, j] = new Cell((i + 1) * (j + 1) - 1, (minX + x_Size / 2) + (j * x_Size), (maxZ - z_Size / 2) - (i * z_Size), Instantiate(tempObj)); //tempObj delete
             }
         }
     }
 
-    public Cell FindCell(int idx)
+    //index에 해당하는 Cell 검색
+    public Cell FindCellByIndex(int idx)
     {
         for (int i=0;i<5;i++)
         {
@@ -43,41 +54,55 @@ public class OfficeArea : MonoBehaviour
 
         return null;
     }
-
-    public int FIndCellIndex(Cell cell)
+    
+    public void SearchOverTheCellObjectAndAddObjectToCell(Cell cell, GameObject obj)
     {
-        for(int i=0;i<5;i++)
+        ObjectConfig config = obj.GetComponent<ObjectConfig>();
+        float horizontalLength = config.getHorizontalLength();
+        float verticalLength = config.getVerticalLength();
+
+        int idx = cell.getIdx();
+
+        int x_count = (int)(horizontalLength / x_Size);
+        int y_count = (int)(verticalLength / z_Size);
+
+        for(int i=0;i<x_count;i++)
         {
-            for(int j=0;j<8;j++)
-            {
-                if (cells[i, j] == cell) return cells[i, j].getIdx();
-            }
+
         }
 
-        return -1;
+        for(int j=0;j<y_count;j++)
+        {
+
+        }
     }
 
-    public void DeductionToAgent()
+    //중복 있는 Agent 찾아서 감점
+    public void FindDuplicateCellAndDeductionToObject()
     {
-        List<GameObject> agentList;
+        List<GameObject> objList;
 
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 8; j++)
             {
-                if (cells[i, j].getAgentCount() > 1)
+                //Cell의 object개수가 2개 이상이면
+                if (cells[i, j].getObjectCount() > 1)
                 {
-                    agentList = cells[i, j].getAgentList();
+                    //List를 받아와서
+                    objList = cells[i, j].getObjectList();
 
-                    for (int k = 0; k < agentList.Count; k++)
+                    //List에 존재하는 모든 Object의 Agent에 1점 감점
+                    for (int k = 0; k < objList.Count; k++)
                     {
-                        agentList[k].GetComponent<OfficeAgent>().AddReward(-1f * cells[i, j].getAgentCount());
+                        objList[k].GetComponent<OfficeAgent>().AddReward(-1f);
                     }
                 }
             }
         }
     }
 
+    /*
     public void AddObject(Cell cell, GameObject agent)
     {
         cell.AddObject(agent);
@@ -86,7 +111,7 @@ public class OfficeArea : MonoBehaviour
         int xLen = (int)(config.x_Length / x_diff);
         int zLen = (int)(config.y_Length / z_diff);
 
-        int idx = FIndCellIndex(cell);
+        int idx = cell.getIdx();
 
         Cell tempCell;
         for (int i = 0; i < xLen; i++)
@@ -107,4 +132,5 @@ public class OfficeArea : MonoBehaviour
             tempCell.AddObject(agent);
         }
     }
+    */
 }
