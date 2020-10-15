77f0b53fd5e31fcf9bd1d4f805b144d19a4e85c9
usangbong <49114064+usangbong@users.noreply.github.com>
2020-10-15 23:19:29 +0900

    Add files via upload

--- gaze_project/gazeBehavior/ver_4/templates/src/components/Menu.js
+++ gaze_project/gazeBehavior/ver_4/templates/src/components/Menu.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import MenuItem from 'components/MenuItem';
 
+
 class Menu extends React.Component {
+
+  onMenuClick = (page) => {
+    this.props.onClick(page);
+  }
+
   render() {
+    const { page } = this.props;
+
     return (
       <ul className="menu-list">
         <li className="menu-item-wrapper">
-          <MenuItem href="/data" title="Data">+</MenuItem>
+          <MenuItem href="/data" title="Data" currentPage={page} onClick={this.onMenuClick}>+</MenuItem>
         </li>
         <li className="menu-item-wrapper">
-          <MenuItem href="/spatial-variance" title="Spatial Variance">S</MenuItem>
+          <MenuItem href="/spatial-variance" title="Spatial Variance" currentPage={page} onClick={this.onMenuClick}>S</MenuItem>
         </li>
         <li className="menu-item-wrapper">
-          <MenuItem href="/analysis" title="Analysis view">A</MenuItem>
+          <MenuItem href="/analysis" title="Analysis view" currentPage={page} onClick={this.onMenuClick}>A</MenuItem>
         </li>
         <li className="menu-item-wrapper">
-          <MenuItem href="/visualization" title="Visualization view">V</MenuItem>
+          <MenuItem href="/visualization" title="Visualization view" currentPage={page} onClick={this.onMenuClick}>V</MenuItem>
         </li>
       </ul>
     );
