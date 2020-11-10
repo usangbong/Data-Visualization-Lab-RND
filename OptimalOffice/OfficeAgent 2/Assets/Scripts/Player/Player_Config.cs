c41636b29673be55f1a3e09d0129569f8ccfff71
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2020-11-10 17:29:57 +0900

    Player 환경 설정

---
+++ OptimalOffice/OfficeAgent 2/Assets/Scripts/Player/Player_Config.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Config : MonoBehaviour
+{
+    public float horizontalLength;
+    public float verticalLength;
+}
