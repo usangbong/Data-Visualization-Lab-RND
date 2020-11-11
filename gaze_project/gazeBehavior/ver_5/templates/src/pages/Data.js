61ba290a9824174fcadce616aed1c0e63a8ef463
usangbong <49114064+usangbong@users.noreply.github.com>
2020-11-12 00:48:35 +0900

    Add files via upload

--- gaze_project/gazeBehavior/ver_5/templates/src/pages/Data.js
+++ gaze_project/gazeBehavior/ver_5/templates/src/pages/Data.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Select from 'react-select';
 import axios from 'axios';
 
+import Heatmap from 'components/Heatmap';
+import CorrelationMatrix from 'components/CorrelationMatrix';
+
+const dataProcessingMethods = [
+  { value:'min_max', label: 'min-max' },
+  { value: 'z_score', label: 'z-score'}
+];
+
 class Data extends React.Component {
   constructor(props) {
     super(props);
@@ -11,9 +19,12 @@ class Data extends React.Component {
       participants: [],
       featureTypes: [],
       stimulusTypes: [],
+      spHeatmapDataURL: "",
+      corrMatDataURL: "",
       selectedDataset: null,
       selectedParticipant: null,
-      selectedFilter: null
+      selectedFilter: null,
+      selectedProcessMethod: null,
     };
   }
 
@@ -61,6 +72,30 @@ class Data extends React.Component {
         console.log(participants_options);
         this.setState({
           participants: participants_options
+        });
+      });
+  }
+
+  loadSPMeanPath = () =>{
+    axios.get(`http://${window.location.hostname}:5000/static/access/sp_heatmap_path.json?`+Math.random())
+      .then(response => {
+        // console.log(response);
+        let _path = "http://"+window.location.hostname+":5000"+response.data;
+        console.log("sp heatmap data access path: "+_path);
+        this.setState({
+          spHeatmapDataURL: _path
+        });
+      });
+  }
+  
+  loadCorrMatrixPath = () =>{
+    axios.get(`http://${window.location.hostname}:5000/static/access/corr_matrix_short_path.json?`+Math.random())
+      .then(response => {
+        // console.log(response);
+        let _path = "http://"+window.location.hostname+":5000"+response.data;
+        console.log("correlation data access path: "+_path);
+        this.setState({
+          corrMatDataURL: _path
         });
       });
   }
@@ -128,19 +163,23 @@ class Data extends React.Component {
     this.loadStimulusTypes(dataName);
   }
 
+  
+
   onSubmit = e => {
     e.preventDefault();
     
     const data = new FormData(e.target);
-    data.set('dataset', this.state.selectedDataset);
-    data.set('participant', this.state.selectedParticipant);
-    data.set('filter', this.state.selectedFilter);
+    data.set('dataset', this.state.selectedDataset.value);
+    data.set('participant', this.state.selectedParticipant.value);
+    data.set('filter', this.state.selectedFilter.value);
 
     axios.post(`http://${window.location.hostname}:5000/api/gaze_data/submit`, data)
       .then(response => {
         if (response.data.status === 'success') {
+          this.loadSPMeanPath();
           alert('Data loaded');
           console.log(response.data);
+          this.loadCorrMatrixPath();
         } else if (response.data.status === 'failed') {
           alert(`Failed to load data - ${response.data.reason}`);
         }
@@ -149,28 +188,42 @@ class Data extends React.Component {
       });
   }
 
-  datasetChange = d => {
-    const selected = d.value;
-    this.setState({selectedDataset: selected});
-    console.log(this.state.selectedDataset);
-    this.loadParticipantsList(selected);
+  datasetChange = selectedDataset => {
+    this.setState({selectedDataset});
+    // console.log(selectedDataset.value);
+    this.loadParticipantsList(selectedDataset.value);
   };
 
-  participantChange = p => {
-    const selected = p.value;
-    this.setState({selectedParticipant: selected});
-    console.log(this.state.selectedParticipant);
+  participantChange = selectedParticipant => {
+    this.setState({selectedParticipant});
+    // console.log(selectedParticipant.value);
   }
 
-  filterChange = f => {
-    const selected = f.value;
-    this.setState({selectedFilter: selected});
-    console.log(this.state.selectedFilter);
-
+  filterChange = selectedFilter => {
+    this.setState({selectedFilter});
+    // console.log(selectedFilter.value);
   };
 
+  pMethodChange = selectedProcessMethod => {
+    this.setState({selectedProcessMethod});
+    
+    const data = new FormData();
+    data.set('processing', selectedProcessMethod.value);
+    axios.post(`http://${window.location.hostname}:5000/api/corr/process`, data)
+    .then(response => {
+      if (response.data.status === 'success') {
+        alert('Data pre-processing aplied');
+        this.loadCorrMatrixPath();
+      } else if (response.data.status === 'failed') {
+        alert(`Failed data pre-processing - ${response.data.reason}`);
+      }
+    }).catch(error => {
+      alert(`Error - ${error.message}`);
+    });
+  }
+
   render() {
-    const { datasetList, participants, fixationFilters, selectedDataset, selectedParticipant, selectedFilter } = this.state;
+    const { datasetList, participants, fixationFilters, selectedDataset, spHeatmapDataURL, corrMatDataURL, selectedParticipant, selectedFilter, selectedProcessMethod } = this.state;
 
     return (
       <>
@@ -207,15 +260,38 @@ class Data extends React.Component {
               options={fixationFilters}
               placeholder="Select eye movement event filter"
             />
-
           </div>
-
-          
 
           <div className="page-section button-wrapper">
             <button className="submit-button">Load</button>
           </div>
         </form>
+
+        {spHeatmapDataURL.length > 0 &&
+          <div id="heat">
+            <Heatmap 
+              dataURL={spHeatmapDataURL}
+            />
+          </div>
+        }
+
+        <div className="page-section select-process">
+          <Select
+            value={selectedProcessMethod}
+            onChange={this.pMethodChange}
+            options={dataProcessingMethods}
+            placeholder="Select data pre-processing method"
+          />
+        </div>
+
+
+        {corrMatDataURL.length > 0 &&
+          <div id="corr">
+            <CorrelationMatrix 
+              dataURL={corrMatDataURL}
+            />
+          </div>  
+        }
       </>
     );
   }
