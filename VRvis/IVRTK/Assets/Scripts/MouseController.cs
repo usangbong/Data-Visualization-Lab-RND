874663644c4d72e6d7c0eb163c1737314602ff64
soobin99 <tn12qls@gmail.com>
2021-01-05 16:03:55 +0900

    폴더정리

---
+++ VRvis/IVRTK/Assets/Scripts/MouseController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public GameObject obj;
+    bool isDown;
+
+    public float moveSpeed = 1f;
+    public float rotateSpeed = 1f;
+    void Start()
+    {
+        isDown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 wheelInput = Input.mouseScrollDelta;
+        if (Input.GetMouseButton(0))
+        {
+            obj.transform.rotation = Quaternion.Euler(new Vector3(obj.transform.rotation.eulerAngles.x, obj.transform.rotation.eulerAngles.y + rotateSpeed, obj.transform.rotation.eulerAngles.z));
+        }
+        if (Input.GetMouseButton(1))
+        {
+            obj.transform.rotation = Quaternion.Euler(new Vector3(obj.transform.rotation.eulerAngles.x, obj.transform.rotation.eulerAngles.y - rotateSpeed, obj.transform.rotation.eulerAngles.z));
+        }
+        if (Input.GetMouseButtonDown(2)) {
+            isDown = true;
+        }
+        if (Input.GetMouseButtonUp(2))
+        {
+            isDown = false;
+        }
+        if (!isDown)
+        {
+            if (wheelInput.y > 0)
+            {
+                obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y , obj.transform.position.z + moveSpeed);
+            }
+            else if (wheelInput.y < 0)
+            {
+                obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y , obj.transform.position.z - moveSpeed);
+            }
+        }
+        else
+        {
+            float rotateSpeed2 = rotateSpeed * 5f;
+            if (wheelInput.y > 0)
+            {
+                obj.transform.rotation = Quaternion.Euler(new Vector3(obj.transform.rotation.eulerAngles.x + rotateSpeed2, obj.transform.rotation.eulerAngles.y, obj.transform.rotation.eulerAngles.z));
+            }
+            else if (wheelInput.y < 0)
+            {
+                obj.transform.rotation = Quaternion.Euler(new Vector3(obj.transform.rotation.eulerAngles.x - rotateSpeed2, obj.transform.rotation.eulerAngles.y, obj.transform.rotation.eulerAngles.z));              
+            }
+        }
+        if (Input.GetMouseButton(3))
+        {
+            obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y - moveSpeed, obj.transform.position.z);
+        }
+        if (Input.GetMouseButton(4))
+        {
+            obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y + moveSpeed, obj.transform.position.z);
+        }
+    }
+}
---
+++ VRvis/IVRTK/Assets/VC/Piechart/Script/PartProperties.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using cakeslice;
+namespace PieChart.ViitorCloud
+{
+    public class PartProperties : MonoBehaviour
+    {
+        public Vector3 forward;
+        public GameObject MyDetailObject;
+        bool animDetailObject;
+        bool lerpYourself;
+        Color color;
+        public float speed = 1.19f;
+
+        public Vector3 MyCurrntPosition;
+        Material material;
+        public void SetInit()
+        {
+            forward = transform.TransformPoint(GetMidPont(GetComponent<MeshCollider>()));
+            forward = new Vector3(forward.x, forward.y, transform.position.z);
+            if (gameObject.GetComponent<Outline>() == null)
+                gameObject.AddComponent<Outline>();
+            GetComponent<Outline>().enabled = false;
+            //GameObject game = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //game.transform.localScale = (Vector3.one) * 0.3f;
+            //GameObject detailibj = Instantiate(game, forward, Quaternion.identity);
+            //material = detailibj.GetComponent<MeshRenderer>().material;
+            //material.color = GetComponent<MeshRenderer>().material.color;
+            //Destroy(game);
+        }
+
+        public void SetAnimation(bool mat)
+        {
+            if (MyDetailObject == null)
+                return;
+            color = MyDetailObject.GetComponent<UnityEngine.UI.Image>().color;
+            animDetailObject = mat;
+
+            if (!mat)
+            {
+                color.a = 1;
+                MyDetailObject.GetComponent<UnityEngine.UI.Image>().color = color;
+            }
+        }
+        void Update()
+        {
+            if (animDetailObject)
+            {
+                color.a = Mathf.PingPong(Time.time * speed, 1);
+                MyDetailObject.GetComponent<UnityEngine.UI.Image>().color = color;
+            }
+            if (lerpYourself)
+            {
+
+            }
+        }
+        public static Vector3 GetMidPont(MeshCollider m_Collider)
+        {
+            return Distinct(m_Collider.sharedMesh.vertices)[Distinct(m_Collider.sharedMesh.vertices).Length / 2];
+            Vector3[] Distinct(Vector3[] handles) => handles.ToList().Distinct().ToArray();
+        }
+    }
+}
