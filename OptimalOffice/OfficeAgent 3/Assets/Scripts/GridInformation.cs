39e6cfb101491d41d001856ed1344be59df819df
ghk71 <38937802+ghk71@users.noreply.github.com>
2021-01-21 20:15:20 +0900

    Reward Function + A-Start 알고리즘 Script

---
+++ OptimalOffice/OfficeAgent 3/Assets/Scripts/GridInformation.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridInformation : MonoBehaviour
+{
+    public Transform gridParent;
+
+    List<List<Cell>> grid;
+    List<List<int>> map;
+
+    private void Awake()
+    {
+        grid = new List<List<Cell>>();
+        map = new List<List<int>>();
+
+        for (var i = 0; i < gridParent.childCount; i++)
+        {
+            map.Add(new List<int>());
+            grid.Add(new List<Cell>());
+
+            for(var j=0;j<gridParent.GetChild(i).childCount;j++)
+            {
+                grid[i].Add(gridParent.GetChild(i).GetChild(j).GetComponent<Cell>());
+            }
+        }
+
+        initializeMap();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public void setMap(List<Cell> markingCell)
+    {
+        
+    }
+
+    public List<Cell> getPath(Cell startNode, Cell endNode)
+    {
+        List<Cell> path = new List<Cell>();
+
+        return path;
+    }
+
+    public float extractDistance(List<Cell> path)
+    {
+        float distance = 0;
+
+        return distance;
+    }
+
+    public void initializeMap()
+    {
+        for(var i=0;i<grid.Count;i++)
+        {
+            for(var j=0;j<grid[i].Count;j++)
+            {
+                map[i][j] = 0;
+                grid[i][j].removeObject();
+            }
+        }
+    }
+}
---
+++ OptimalOffice/OfficeAgent 3/Assets/Scripts/Real/Map.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public Transform MapObj;
+
+    private List<List<int>> map;
+    private List<List<Transform>> mapObjList;
+
+    private void Awake()
+    {
+        map = new List<List<int>>();
+        mapObjList = new List<List<Transform>>();
+
+        for (var i = 0; i < 12; i++)
+        {
+            map.Add(new List<int>());
+            for (var j = 0; j < 12; j++)
+            {
+                map[i].Add(0);
+            }
+        }
+
+        for (var i = 0; i < 12; i++)
+        {
+            mapObjList.Add(new List<Transform>());
+            for (var j = 0; j < 12; j++)
+            {
+                mapObjList[i].Add(MapObj.GetChild(i).GetChild(j));
+                mapObjList[i][j].GetComponent<MeshRenderer>().material.color = Color.white;
+            }
+        }
+    }
+
+    public void fillMap(int x, int z, int xSize, int zSize, Color color)
+    {
+        for (var i = z; i < z + zSize; i++)
+        {
+            for (var j = x; j < x + xSize; j++)
+            {
+                map[i][j] = 1;
+                mapObjList[i][j].GetComponent<MeshRenderer>().material.color = color;
+            }
+        }
+    }
+
+    public bool canFillMap(int x, int z, int xSize, int zSize)
+    {
+        if (x + xSize > 12 || x < 0) return false;
+        if (z + zSize > 12 || z < 0) return false;
+
+        for (var i = z; i < z + zSize; i++)
+        {
+            for (var j = x; j < x + xSize; j++)
+            {
+                if (map[i][j] == 1) return false;
+            }
+        }
+
+        return true;
+    }    
+
+    public void clearMap()
+    {
+        for (var i = 0; i < 12; i++)
+        {
+            for (var j = 0; j < 12; j++)
+            {
+                map[i][j] = 0;
+                mapObjList[i][j].GetComponent<MeshRenderer>().material.color = Color.white;
+            }
+        }
+    }
+
+    public void deleteMap(int x, int z, int xSize, int zSize)
+    {
+        for (var i = z; i < z + zSize; i++)
+        {
+            for (var j = x; j < x + xSize; j++)
+            {
+                map[i][j] = 0;
+                mapObjList[i][j].GetComponent<MeshRenderer>().material.color = Color.white;
+            }
+        }
+    }
+}
