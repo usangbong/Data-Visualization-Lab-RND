360903e6dc039fd67b3d4f1181de2c7e4f666bdf
usangbong <49114064+usangbong@users.noreply.github.com>
2021-02-07 13:31:53 +0900

    Add files via upload

---
+++ gaze_project/gazeBehavior/ver_5/templates/src/components/ClusteringScatterPlot.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef } from 'react';
+
+function ClusteringScatterPlot(props) {
+  const { width, height, dataURL } = props;
+  const svgRef = useRef();
+  const d3 = window.d3;
+  const colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33", "#a65628", "#f781bf", "#999999"];
+
+  useEffect(() => {
+    if(typeof dataURL !== 'string' || dataURL.length === 0)
+      return;
+
+    d3.select(svgRef.current).selectAll("*").remove();
+    
+      // set the dimensions and margins of the graph
+    var margin = {top: 30, right: 30, bottom: 30, left: 30},
+    drawWidth = width - margin.left - margin.right,
+    drawHeight = height - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select(svgRef.current)
+    .attr("width", drawWidth + margin.left + margin.right)
+    .attr("height", drawHeight + margin.top + margin.bottom)
+    .append("svg")
+    .attr("width", drawWidth + margin.left + margin.right)
+    .attr("height", drawHeight + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+    //Read the data
+    d3.csv(dataURL, function(data) {
+      // console.log(data);
+      // Add X axis
+      var xMin = d3.min(data, (d => parseFloat(d.x)));
+      var xMax = d3.max(data, (d => parseFloat(d.x)));
+      var x = d3.scaleLinear()
+        .domain([xMin, xMax])
+        .range([ 0, drawWidth ]);
+      svg.append("g")
+        .attr("class", "xaxis")
+        .attr("transform", "translate(0," + drawHeight + ")")
+        .call(d3.axisBottom(x));
+
+      // Add Y axis
+      var yMin = d3.min(data, (d => parseFloat(d.y)));
+      var yMax = d3.max(data, (d => parseFloat(d.y)));
+      var y = d3.scaleLinear()
+        .domain([yMin, yMax])
+        .range([ drawHeight, 0]);
+      svg.append("g")
+        .attr("class", "yaxis")
+        .call(d3.axisLeft(y));
+
+      // Add dots
+      svg.append('g')
+      .selectAll("dot")
+      .data(data)
+      .enter()
+      .append("circle")
+        .attr("class", "point")
+        .attr("cx", function (d) { return x(d.x); } )
+        .attr("cy", function (d) { return y(d.y); } )
+        .attr("r", 1.5)
+        .style("fill", function (d) {return colors[parseInt(d.clu)]});
+
+        d3.selectAll('.point')
+        .on('click', function(d){
+          console.log(d);
+        });
+    });
+    
+    
+  }, [, props.dataURL]);
+
+  return (
+    <>
+      {typeof dataURL === 'string' && dataURL.length !== 0 &&
+        <svg ref={svgRef}>
+        </svg>
+      }
+    </>
+  );
+}
+
+export default ClusteringScatterPlot;
