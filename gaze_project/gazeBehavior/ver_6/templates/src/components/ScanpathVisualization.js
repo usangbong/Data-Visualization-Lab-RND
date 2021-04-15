748452f6c90e096b9c7a1029bb622fa20521ac11
usangbong <49114064+usangbong@users.noreply.github.com>
2021-04-16 00:10:19 +0900

    Add files via upload

--- gaze_project/gazeBehavior/ver_6/templates/src/components/ScanpathVisualization.js
+++ gaze_project/gazeBehavior/ver_6/templates/src/components/ScanpathVisualization.js
@@ -10,7 +10,7 @@ function ScanpathVisualization(props) {
   const d3 = window.d3;
   // const colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33", "#a65628", "#f781bf", "#999999"];
   useEffect(() => {
-    console.log(colorEncoding);
+    // console.log(colorEncoding);
     if(stimulusURL.length == 0)
       return;
     d3.select(svgRef.current).selectAll("*").remove();
