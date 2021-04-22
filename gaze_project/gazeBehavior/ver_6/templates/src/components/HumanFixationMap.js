87cd0e00d29a399af32ce0992b376de24ff0034b
usangbong <49114064+usangbong@users.noreply.github.com>
2021-04-23 06:33:26 +0900

    Add files via upload

--- gaze_project/gazeBehavior/ver_6/templates/src/components/HumanFixationMap.js
+++ gaze_project/gazeBehavior/ver_6/templates/src/components/HumanFixationMap.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 
 function HumanFixationMap(props) {
-  const { width, height, humanFixationMapURL, patchDataList } = props;
+  const { width, height, humanFixationMapURL, patchDataList, colorEncoding, patchDrawFlag, selectedObserver, patchBoxOpacity } = props;
   const svgRef = useRef();
-  const d3 = window.d3;
+  const d3 = window.d3v4;
+  const colores_g = ["#109618", "#ff9900", "#990099", "#dd4477", "#0099c6", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac", "#3366cc", "#dc3912"];
   const PATCH_SIZE = 20;
+  const PATCH_DRAW_LENGTH = 8;
   
   useEffect(() => {
     if(humanFixationMapURL.length == 0)
@@ -30,30 +32,48 @@ function HumanFixationMap(props) {
     }
     // console.log("RAW DATA LIST");
     // console.log(patchDataList);
+    let allObserverList = [];
+    for(let i=0; i<patchDataList.length; i++){
+      allObserverList.push(patchDataList[i][0].split("/")[3]);
+    }
+    let set = new Set(allObserverList);
+    let observerList = [...set];
 
     let patchData = [];
     for(let i=0; i<patchDataList.length; i++){
+      let _id = patchDataList[i][0].split("/")[3];
+      let _idIdx = 0;
+      for(let j=0; j<observerList.length; j++){
+        if(_id == observerList[j]){
+          _idIdx = j;
+          break;
+        }
+      }
       let box = {
+        id: _idIdx,
         x: parseFloat((patchDataList[i][1]-PATCH_SIZE/2)*ratio),
         y: parseFloat((patchDataList[i][2]-PATCH_SIZE/2)*ratio),
-        label: patchDataList[i][3]
+        label: patchDataList[i][3],
+        observer: _id
       };
       patchData.push(box);
     }
+    // console.log("patchData");
+    // console.log(patchData);
 
     var url_rnd = humanFixationMapURL[0].url+"?"+Math.random();
     var svg = d3.select(svgRef.current)
-    .attr("width", drawWidth + margin.left + margin.right)
-    .attr("height", drawHeight + margin.top + margin.bottom)
+    .attr("width", width)
+    .attr("height", height)
       .append("svg")
-      .attr("width", drawWidth + margin.left + margin.right)
-      .attr("height", drawHeight + margin.top + margin.bottom)
+      .attr("width", width)
+      .attr("height", height)
       .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform", "translate(0, 0)");
 
     svg.append("image")
-    .attr('x', 0)
-    .attr('y', 0)
+    .attr('x', margin.left)
+    .attr('y', margin.top)
     .attr("width", stiWidth)
     .attr("height", stiHeight)
     .attr("xlink:href", url_rnd);
@@ -65,27 +85,49 @@ function HumanFixationMap(props) {
     .append("g")
     .attr("class", "opFrame")
     .attr("transform", function(d) {
-      return "translate(" + d.x + "," + d.y + ")";
+      let _x = d.x + margin.left - (PATCH_DRAW_LENGTH/2);
+      let _y = d.y + margin.top - (PATCH_DRAW_LENGTH/2);
+      return "translate(" + _x + "," + _y + ")";
     });
 
     patchFrame.append('rect')
-    .attr("width", PATCH_SIZE*ratio)
-    .attr("height", PATCH_SIZE*ratio)
-    .style("fill", "none")
-    .attr("stroke", function(d){
-      if(d.label == 0){
-        return "blue";
-      }else if(d.label == 1){
-        return "red";
+    .attr("width", PATCH_DRAW_LENGTH)
+    .attr("height", PATCH_DRAW_LENGTH)
+    .style("fill", function(d){
+      if(patchDrawFlag == "box"){
+        return colorEncoding[d.label];
+      }else if(patchDrawFlag == "observer"){
+        if(d.id < colores_g.length){
+          return colores_g[d.id];
+        }else{
+          let mIdx = d.id - colores_g.length;
+          return colorEncoding[mIdx];
+        }
       }else{
-        return "black";
+        return "none";
       }
     })
-    .attr("stroke-width", "2px");
+    .attr("stroke", function(d){ return colorEncoding[d.label] })
+    .attr("stroke-width", "2px")
+    .attr("opacity", function(d){
+      if(selectedObserver.length == 0){
+        return patchBoxOpacity;
+      }else{
+        if(selectedObserver[1] == d.observer){
+          return 1;
+        }else{
+          if(patchBoxOpacity == 1){
+            return 0.3;
+          }else{
+            return patchBoxOpacity;
+          }
+        }
+      }
+    });
 
     
     
-  }, [, props.humanFixationMapURL, props.patchDataList]);
+  }, [, props.humanFixationMapURL, props.patchDataList, props.patchDrawFlag, props.patchBoxOpacity, props.selectedObserver]);
 
   return (
     <>
