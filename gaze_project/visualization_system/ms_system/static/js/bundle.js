05a1de11b01a8a8ca87ebc0859f154df4126ec4c
usangbong <49114064+usangbong@users.noreply.github.com>
2021-05-17 22:29:38 +0900

    Add files via upload

---
+++ gaze_project/visualization_system/ms_system/static/js/bundle.js
@@ -0,0 +1,16 @@
+//const pica = require('pica')();
+
+// Resize from Canvas/Image to another Canvas
+/*
+pica.resize(from, to, {
+  unsharpAmount: 80,
+  unsharpRadius: 0.6,
+  unsharpThreshold: 2
+})
+.then(result => console.log('resize done!'));
+
+// Resize & convert to blob
+pica.resize(from, to)
+  .then(result => pica.toBlob(result, 'image/jpeg', 0.90))
+  .then(blob => console.log('resized to canvas & created blob!'));
+  */
