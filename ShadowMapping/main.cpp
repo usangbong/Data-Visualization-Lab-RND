044609aaddb49b99852e8b7a3ba6aa6cf240bc6f
ksy0586 <54608134+ksy0586@users.noreply.github.com>
2021-05-27 15:20:09 +0900

    Update main.cpp

--- ShadowMapping/main.cpp
+++ ShadowMapping/main.cpp
@@ -1,1 +1,26 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
 
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include <common/shader.hpp>
+#include <common/texture.hpp>
+#include <common/controls.hpp>
+#include <common/objloader.hpp>
+#include <common/vboindexer.hpp>
+
+int main( void )
+{
+}
