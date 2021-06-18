88662569335eb21cc02bf493d82b63ca4268d25f
usangbong <49114064+usangbong@users.noreply.github.com>
2021-06-18 10:07:23 +0900

    Update test.py

--- gaze_project/pythons/torch_examples/test.py
+++ gaze_project/pythons/torch_examples/test.py
@@ -4,7 +4,6 @@ model_location = 'models'
 mit_stimuli, mit_fixations = pysaliency.external_datasets.get_mit1003(location=dataset_location)
 aim = pysaliency.AIM(location=model_location)
 saliency_map = aim.saliency_map(mit_stimuli.stimuli[0])
-
 plt.imshow(saliency_map)
 
 auc = aim.AUC(mit_stimuli, mit_fixations)
