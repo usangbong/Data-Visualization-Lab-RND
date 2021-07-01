cea20cda8936b3c67d4bf447e946da3a8897274b
aattee <35211521+aattee@users.noreply.github.com>
2021-07-01 18:49:50 +0900

    Add files via upload

--- 3DBPP/dist_dqn/libs/dqn.py
+++ 3DBPP/dist_dqn/libs/dqn.py
@@ -14,22 +14,25 @@ from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, concat
 # CUDA_VISIBLE_DEVICES=""
 
 class DQN_CNNDNN(tf.keras.Model):
-    def __init__(self, state_size, selected_size, remain_size, loading_size, cbm_size,  output_size):
+    def __init__(self, state_size, selected_size, remain_size, loading_size, cbm_size,  w_size, output_size):
         super(DQN_CNNDNN, self).__init__()
-        self.case_cnn1 = Conv2D(filters=5, kernel_size=3, activation='relu', padding="valid", input_shape = state_size)
+        self.case_cnn1 = Conv2D(filters=16, kernel_size=3, activation='relu', padding="valid", input_shape = state_size) #filters=5,
         self.case_dnn1 = Dense(32, activation='relu')
-        # location - selected boxes
-        self.sel_cnn1 = Conv2D(filters=5, kernel_size=3, activation='relu', padding="valid", input_shape = selected_size)
+        # location - selected boxes 
+        self.sel_cnn1 = Conv2D(filters=16, kernel_size=3, activation='relu', padding="valid", input_shape = selected_size) #filters=5,
         self.sel_dnn1 = Dense(32, activation='relu')
         # size - remain boxes
-        self.r_cnn1 = Conv2D(filters=8, kernel_size=3, activation='relu', padding="valid", input_shape = remain_size )
+        self.r_cnn1 = Conv2D(filters=32, kernel_size=3, activation='relu', padding="valid", input_shape = remain_size ) #filters=8,
         self.r_dnn1 = Dense(64, activation='relu')
         # size - selected boxes
-        self.l_cnn1 = Conv2D(filters=3, kernel_size=(1,3), activation='relu', padding="valid", input_shape = loading_size )
+        self.l_cnn1 = Conv2D(filters=8, kernel_size=(1,3), activation='relu', padding="valid", input_shape = loading_size ) #filters=3, 
         self.l_dnn1 = Dense(32, activation='relu')
         # cbm
         self.cbm_dnn1 = Dense(128, activation='relu', input_shape = cbm_size )
         self.cbm_dnn2 = Dense(32, activation='relu')
+        # weight
+        self.w_dnn1 = Dense(128, activation='relu', input_shape = w_size ) #64, 
+        self.w_dnn2 = Dense(32, activation='relu')
         # all
         #n_ch = state_size[-1] + selected_size[-1] + remain_size[-1] + loading_size[-1] 
         #self.a_cnn1 = Conv2D(filters=8, kernel_size=3, activation='relu', padding="valid", input_shape = state_size[:-1]+(n_ch,))
@@ -44,7 +47,7 @@ class DQN_CNNDNN(tf.keras.Model):
             self.fc_out = Dense(output_size)
 
     def call(self, cb_list):
-        c, s, r,l, cbm = cb_list[0], cb_list[1], cb_list[2], cb_list[3], cb_list[4]
+        c, s, r,l, cbm, w = cb_list[0], cb_list[1], cb_list[2], cb_list[3], cb_list[4], cb_list[5]
         #c, s, r = cb_list[0], cb_list[1], cb_list[2]
         ### case
         c = self.case_cnn1(c)
@@ -69,6 +72,9 @@ class DQN_CNNDNN(tf.keras.Model):
         ### cbm
         cbm = self.cbm_dnn1(cbm)
         cbm = self.cbm_dnn2(cbm)
+        ### weight
+        w = self.w_dnn1(w)
+        w = self.w_dnn2(w)
         ### all
         #a =  tf.concat([c, r, l], -1)
         #a = self.a_cnn1(a)
@@ -76,93 +82,22 @@ class DQN_CNNDNN(tf.keras.Model):
         #a = Flatten()(a)
         #a = self.a_dnn1(a)
         ### merge
-        x = concatenate([c,s,r,l, cbm])
+        x = concatenate([c,s,r,l, cbm, w])
         x = self.fc1(x)
         x = self.fc2(x)
         q = self.fc_out(x)
         return q    
 
-class DQN_DNN(tf.keras.Model):
-    def __init__(self, state_size, selected_size, remain_size, loading_size, cbm_size, output_size):
-        super(DQN_DNN, self).__init__()
-        # case network
-        l1,b1,k1 = state_size # 배치 사이즈 제외된 사이즈
-        self.state_size = (l1*b1*k1,)
-        self.case_dnn1 = Dense(64, activation='relu',input_shape = self.state_size)
-        self.case_dnn2 = Dense(64, activation='relu')#
-        # location - selected boxes
-        l2,b2,k2 = selected_size
-        self.selected_size = (l2*b2*k2,)
-        self.sel_dnn1 = Dense(64, activation='relu',input_shape = self.selected_size)
-        self.sel_dnn2 = Dense(64, activation='relu')#
-        # remain boxes
-        l3,b3,k3 = remain_size
-        self.remain_size = (l3*b3*k3, )
-        self.r_dnn1 = Dense(128, activation='relu', input_shape = self.remain_size  )
-        self.r_dnn2 = Dense(128, activation='relu')
-        # loading boxes - selected boxes
-        l4,b4,k4 = loading_size
-        self.loading_size = (l4*b4*k4, )
-        self.l_dnn1 = Dense(128, activation='relu', input_shape = self.loading_size  )
-        self.l_dnn2 = Dense(128, activation='relu')
-        # CBM - bboxcbm + selected cbm + remain cbm
-        self.cbm_dnn1 = Dense(128, activation='relu', input_shape = cbm_size  )
-        self.cbm_dnn2 = Dense(128, activation='relu')
-        # all
-        #self.all_size = (self.state_size[0]+self.selected_size[0]+self.remain_size[0]+self.loading_size[0], )
-        #self.a_dnn1 = Dense(128, activation='relu', input_shape = self.all_size  )
-        # merge 
-        self.fc1 = Dense(256, activation='relu')
-        self.fc2 = Dense(256, activation='relu')
-        self.fc2 = Dense(128, activation='relu')
-        self.fc_out = Dense(output_size, activation='softmax')
-        if output_size > 1: #DDQN
-            self.fc_out = Dense(output_size, activation='softmax')
-        else:
-            self.fc_out = Dense(output_size)
-
-    def call(self, cb_list):
-        c, s, r, l,cbm = cb_list[0], cb_list[1], cb_list[2], cb_list[3], cb_list[4]
-        c = tf.reshape(c, [-1, self.state_size[0]])
-        s = tf.reshape(s, [-1, self.selected_size[0]])
-        r = tf.reshape(r, [-1, self.remain_size[0]])
-        l = tf.reshape(l, [-1, self.loading_size[0]])
-        #a =  tf.concat([c, s, r, l], -1)
-        ### case
-        c = self.case_dnn1(c)
-        c = self.case_dnn2(c)
-        ### location - selected boxes
-        s = self.sel_dnn1(s)
-        s = self.sel_dnn2(s)
-        ### remain boxes
-        r = self.r_dnn1(r)
-        r = self.r_dnn2(r)
-        ### loading boxes
-        l = self.l_dnn1(l)
-        l = self.l_dnn2(l)
-        ### cbm
-        cbm = self.cbm_dnn1(cbm)
-        cbm = self.cbm_dnn2(cbm)
-        ### all
-        #a = self.a_dnn1(a)
-        ### merge
-        x = concatenate([c,s,r,l, cbm])#x = concatenate([c,s,r,l,a])
-        x = self.fc1(x)
-        x = self.fc2(x)
-        q = self.fc_out(x)
-        return q
-
-
-
 class DQNAgent:
-    def __init__(self, L=20, B=20, H=20, n_remains = 5, n_loading=3, n_channel = 6, max_size = 64,
+    def __init__(self, L=20, B=20, H=20, n_remains = 5, n_loading=3, n_channel = 4, max_size = 64,
                  lr=1e-8, batch_size = 32, exp_steps=500, train_st = 200, memory_len=500, update_target_rate = 30, net='DNN' ):
-        self.state_size = (L, B, 3)
-        self.selected_size = (L, B, n_loading)
-        self.remain_size = (n_remains, max_size ,n_channel) #(L, B, n_remains)
-        self.loading_size = (n_loading, max_size ,n_channel//2) # (L, B, n_loading)
-        self.cbm_size = (1 + n_remains*2 + n_loading,)
-        self.output_size = 1 #math.factorial(c_boxes_size)
+        self.state_size = (L, B, 4) # 합, 높이, 대박스 규격, 무게
+        self.selected_size = (L, B, n_loading*2) #적입할 중박스의 위치와 무게
+        self.remain_size = (n_remains, max_size ,n_channel) # 남은 중박스 규격(세로가로), 적입된 중박스 규격(세로가로)
+        self.loading_size = (n_loading, max_size ,n_channel//2) # 적입할 중박스 규격(세로가로)
+        self.cbm_size = (1 + n_remains*2 + n_loading,) #대박스 CBM, 남은 중박스 CBM, 적입된 중박스 CBM, 적입할 중박스 CBM 
+        self.w_size = (n_remains + n_loading + 8,) # 남은 중박스 무게, 적입할 중박스 무게
+        self.output_size = 1 # DQN output (dist 아님)
         # hyperparameters
         self.discount_factor = 0.99
         self.learning_rate = lr#1e-8#1e-4
@@ -200,8 +135,8 @@ class DQNAgent:
             self.model = DQN_DNN(self.state_size, self.selected_size, self.remain_size, self.loading_size, self.cbm_size, self.nsup)
             self.target_model = DQN_DNN(self.state_size, self.selected_size, self.remain_size, self.loading_size, self.cbm_size, self.nsup)
         elif net == 'DDQN_CNNDNN':
-            self.model = DQN_CNNDNN(self.state_size, self.selected_size, self.remain_size, self.loading_size, self.cbm_size, self.nsup)
-            self.target_model = DQN_CNNDNN(self.state_size, self.selected_size, self.remain_size, self.loading_size, self.cbm_size, self.nsup)
+            self.model = DQN_CNNDNN(self.state_size, self.selected_size, self.remain_size, self.loading_size, self.cbm_size, self.w_size, self.nsup)
+            self.target_model = DQN_CNNDNN(self.state_size, self.selected_size, self.remain_size, self.loading_size, self.cbm_size, self.w_size, self.nsup)
         
         if net in ['DDQN_DNN', 'DDQN_CNNDNN']:
             print('distribution', net)
@@ -217,27 +152,27 @@ class DQNAgent:
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
 
-    def get_action(self, state, loaded_mh_c, r_boxes, loading, cbm):
+    def get_action(self, state, loaded_mh_c, r_boxes, loading, cbm, w):
         if np.random.rand() <= self.epsilon:
             random_action = random.randrange(len(state))
             return random_action
         else:
             if self.dist:
-                z = self.model.predict([state, loaded_mh_c, r_boxes, loading, cbm]) #(C,51)
+                z = self.model.predict([state, loaded_mh_c, r_boxes, loading, cbm, w]) #(C,51)
                 z_concat = np.vstack(z) #action output이 여러개 일때
                 #q = np.sum(np.multiply(z_concat[:,45:], np.array(self.z)[45:]), axis=1) #(C,)
                 q = np.sum(np.multiply(z_concat, np.array(self.z)), axis=1) #(C,)
                 self.avg_q_max += np.amax(q)
                 return np.argmax(q)
             else:
-                q_values=self.model([state, loaded_mh_c, r_boxes, loading, cbm])
+                q_values=self.model([state, loaded_mh_c, r_boxes, loading, cbm, w])
                 armax_idx = np.where( q_values == tf.math.reduce_max(q_values))
                 action_idx = armax_idx[0][0]
                 return action_idx
     
    
-    def append_sample(self, history, load, remain_size, load_size, reward, last, t_history, t_load, t_remain_size, t_load_size, cbm,  t_cbm):
-        self.memory.append(( history, load, remain_size, load_size, reward, last, t_history, t_load, t_remain_size, t_load_size, cbm, t_cbm))
+    def append_sample(self, history, load, remain_size, load_size, reward, last, t_history, t_load, t_remain_size, t_load_size, cbm,  t_cbm, w, t_w):
+        self.memory.append(( history, load, remain_size, load_size, reward, last, t_history, t_load, t_remain_size, t_load_size, cbm, t_cbm, w, t_w))
         
         
 #     def draw_tensorboard(self, reward, fill, step, episode):
@@ -265,13 +200,16 @@ class DQNAgent:
         t_load_size = np.concatenate([sample[9] for sample in batch] )
         cbm = np.array([sample[10] for sample in batch] )
         t_cbm = np.concatenate([sample[11] for sample in batch] )
+        w = np.array([sample[12] for sample in batch] )
+        t_w = np.concatenate([sample[13] for sample in batch] )
+        
         #print(history.shape,load.shape,remain_size.shape, load_size.shape, reward.shape, dones.shape,  t_history.shape, t_load.shape, t_remain_size.shape, t_load_size.shape)
         
         model_params = self.model.trainable_variables
         with tf.GradientTape() as tape:
             if self.dist:
                 # 예측
-                predicts = self.model([history, load, remain_size, load_size, cbm]) # B,51
+                predicts = self.model([history, load, remain_size, load_size, cbm, w]) # B,51
                 # 타겟
 #                 z_b = []
 #                 for i in range(0,len(t_history), self.batch_size):
@@ -279,7 +217,7 @@ class DQNAgent:
 #                     z = self.target_model([t_history[i:e_idx], t_load[i:e_idx], t_remain_size[i:e_idx], t_load_size[i:e_idx]]) #(C, 51)
 #                     z_b.append(z)
 #                 z_concat = np.vstack(z_b) #(B*c, 51)
-                z_concat = self.target_model([t_history, t_load, t_remain_size, t_load_size, t_cbm])
+                z_concat = self.target_model([t_history, t_load, t_remain_size, t_load_size, t_cbm, t_w])
                 t_q = np.sum(np.multiply(z_concat, np.array(self.z)), axis=1) #(B*c, )
                 # 가장 기댓값이 높은 action 선택
                 next_actions, probs = [],[]
@@ -321,7 +259,7 @@ class DQNAgent:
                 # 타겟
                 targets = []
                 for i in range(self.batch_size):
-                    t_q = self.target_model([t_history[i], t_load[i], t_remain_size[i], t_load_size[i], t_cbm[i]]) # target q value
+                    t_q = self.target_model([t_history[i], t_load[i], t_remain_size[i], t_load_size[i], t_cbm[i], t_w[i]]) # target q value
                     t_max_q = tf.math.reduce_max(t_q)
                     targets.append([(1- 0.75)*reward[i] + (1 - dones[i]) *0.75*t_max_q])
                 targets=np.array(targets) #(B, 1)
