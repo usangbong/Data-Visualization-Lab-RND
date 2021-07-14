b0f840e0bc9a35dddcc8b3774609236e44c7276a
usangbong <49114064+usangbong@users.noreply.github.com>
2021-07-14 10:54:38 +0900

    Add files via upload

--- gaze_project/feature_models/log_spectrum/make_log_spectrum_csv.py
+++ gaze_project/feature_models/log_spectrum/make_log_spectrum_csv.py
@@ -18,71 +18,66 @@ def csvWriter(_outputfilename, _data):
     f.close()
 
 
-DIRPATH = "../sti/"
-dir_list = os.listdir(DIRPATH)
+# DIRPATH = "../backup_full/"
+# dir_list = os.listdir(DIRPATH)
+# OUTDIR = "../output/vis_sti_feature/log_spectrum/"
+# create_dir(OUTDIR)
 
-OUT_PATH = "../sti_features/log/"
-create_dir(OUT_PATH)
-
-stiCount = 0
-dirChange = 0
-for dirname in dir_list:
-    if stiCount == 60:
-        break
-    _path = DIRPATH + dirname + "/"
-    sti_list = os.listdir(_path)
-
-    for _sti in sti_list:
-        if dirChange != 0 and dirChange%3 == 0:
-            dirChange = 0
-            break
-        else:
-            print(stiCount)
-        csvPath = OUT_PATH + dirname+ "_" + _sti[:-4] + ".csv"
-
-        _stiPath = _path + _sti
-        print(_stiPath)
-        img = cv2.imread(_stiPath, 0)
-        WIDTH = img.shape[1]
-        calc = int(WIDTH*img.shape[0]/img.shape[1])
-        img = cv2.resize(img, (WIDTH, calc))
-
-        c = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
-        mag = np.sqrt(c[:,:,0]**2 + c[:,:,1]**2)
-        spectralResidual = np.exp(np.log(mag) - cv2.boxFilter(np.log(mag), -1, (3,3)))
-
-        log_spectrum = np.log(mag)
-        csvWriter(csvPath, log_spectrum)
-
-
-        stiCount += 1
-        dirChange += 1
-print("log spectrum feature extraction done")
-
-
-
-# IMG_DIR = "../vs/"
-# file_list = os.listdir(IMG_DIR)
-
-# OUT_DIR = "./out/"
-# create_dir(OUT_DIR)
-
-# IMG_DIR = "./vs/"
-
-# for IMG_NAME in file_list:
-#     IMG_FULL_PATH = IMG_DIR+IMG_NAME
-#     img = cv2.imread(IMG_FULL_PATH, 0)
-
-#     WIDTH = img.shape[1]
-#     calc = int(WIDTH*img.shape[0]/img.shape[1])
-#     img = cv2.resize(img, (WIDTH, calc))
-
-#     c = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
-#     mag = np.sqrt(c[:,:,0]**2 + c[:,:,1]**2)
-#     spectralResidual = np.exp(np.log(mag) - cv2.boxFilter(np.log(mag), -1, (3,3)))
-
-#     log_spectrum = np.log(mag)
-
-#     OUTPUT_FULL_PATH = OUT_DIR+IMG_NAME[:-4]+"_log_spectrum.csv"
-#     csvWriter(OUTPUT_FULL_PATH, log_spectrum)
+# for dirname in dir_list:
+#     _path = DIRPATH + dirname + "/"
+#     if dirname == 'MIT1003':
+#         _stiNameList = os.listdir(_path)
+#         for _stiName in _stiNameList:
+#             _stiPath = _path + _stiName
+#             print(_stiPath)
+#             img = cv2.imread(_stiPath, 0)
+#             WIDTH = img.shape[1]
+#             calc = int(WIDTH*img.shape[0]/img.shape[1])
+#             img = cv2.resize(img, (WIDTH, calc))
+#             c = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
+#             mag = np.sqrt(c[:,:,0]**2 + c[:,:,1]**2)
+#             spectralResidual = np.exp(np.log(mag) - cv2.boxFilter(np.log(mag), -1, (3,3)))
+#             log_spectrum = np.log(mag)
+            
+#             csvPath = OUTDIR + dirname +"/"
+#             create_dir(csvPath)
+#             csvPath = csvPath + _stiName.split('.')[0]+".csv"
+#             csvWriter(csvPath, log_spectrum)
+#     else:
+#         _stiClassList = os.listdir(_path)
+#         for _stiClass in _stiClassList:
+#             _stiPath = _path + _stiClass + "/"
+#             _stiNameList = os.listdir(_stiPath)
+#             for _stiName in _stiNameList:
+#                 _stiPath = _path + _stiClass + "/" + _stiName
+#                 print(_stiPath)
+#                 img = cv2.imread(_stiPath, 0)
+#                 WIDTH = img.shape[1]
+#                 calc = int(WIDTH*img.shape[0]/img.shape[1])
+#                 img = cv2.resize(img, (WIDTH, calc))
+#                 c = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
+#                 mag = np.sqrt(c[:,:,0]**2 + c[:,:,1]**2)
+#                 spectralResidual = np.exp(np.log(mag) - cv2.boxFilter(np.log(mag), -1, (3,3)))
+#                 log_spectrum = np.log(mag)
+                
+#                 csvPath = OUTDIR + dirname +"/"
+#                 create_dir(csvPath)
+#                 csvPath = csvPath + _stiClass +"/"
+#                 create_dir(csvPath)
+#                 csvPath = csvPath + _stiName.split('.')[0]+".csv"
+#                 csvWriter(csvPath, log_spectrum)
 # print("log spectrum feature extraction done")
+
+_stiPath = "./nullschool.png"
+csvPath = "./log_spectrum.csv"
+
+img = cv2.imread(_stiPath, 0)
+WIDTH = img.shape[1]
+calc = int(WIDTH*img.shape[0]/img.shape[1])
+img = cv2.resize(img, (WIDTH, calc))
+c = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
+mag = np.sqrt(c[:,:,0]**2 + c[:,:,1]**2)
+spectralResidual = np.exp(np.log(mag) - cv2.boxFilter(np.log(mag), -1, (3,3)))
+log_spectrum = np.log(mag)
+csvWriter(csvPath, log_spectrum)
+print("log spectrum feature extraction done")
