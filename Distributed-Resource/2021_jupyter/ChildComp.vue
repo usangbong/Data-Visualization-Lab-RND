99df308904e2408032a0adb19183d1fcf4bde646
yeoon-p <72381095+yeoon-p@users.noreply.github.com>
2021-10-01 18:22:11 +0900

    Add files via upload

---
+++ Distributed-Resource/2021_jupyter/ChildComp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="childComp">
+    <div class="container">
+      <p>Child Component</p>
+    </div>
+  </div>  
+</template>
+
+<script>
+/*
+import Vue from 'vue'
+
+Vue.component('button-counter', {
+  template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1
+      this.$emit('increment')
+    }
+  },
+})*/
+</script>
+
+<!--<style lang="scss">-->
+<style scoped>
+.childComp {
+  border: 1px solid;
+  box-sizing: border-box;
+  background:rgb(228, 110, 110) !important;
+}
+</style>
