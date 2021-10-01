40dd35537dd5845d04e3c20b95c9732f00c1717d
yeoon-p <72381095+yeoon-p@users.noreply.github.com>
2021-10-01 18:21:52 +0900

    Add files via upload

---
+++ Distributed-Resource/2021_jupyter/TestComp.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="counter-event-example">
+  </div>
+</template>
+
+<script>
+//import Vue from 'vue'
+//import CildComp from './ChildComp.vue'
+
+/*
+
+Vue.component('button-counter', {
+  
+  template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1
+      this.$emit('increment')
+    }
+  },
+})
+new Vue({
+  el: '#counter-event-example',
+  data: {
+    total: 0
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total += 1
+    }
+  }
+})
+*/
+</script>
+<style lang="scss">
+.TestCompDiv {
+  width: 1870px;
+  height: 20px;
+  margin-left: auto;
+  margin-top: 50px;
+  color: rgb(128, 126, 126);
+}
+</style>
