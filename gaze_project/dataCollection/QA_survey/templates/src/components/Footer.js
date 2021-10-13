aa8cd1e20439619c58194731df45bb2ed13960e5
usangbong <49114064+usangbong@users.noreply.github.com>
2021-10-13 20:48:48 +0900

    Add files via upload

--- gaze_project/dataCollection/QA_survey/templates/src/components/Footer.js
+++ gaze_project/dataCollection/QA_survey/templates/src/components/Footer.js
@@ -1,1 +1,15 @@
+import React from 'react';
 
+class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <div>ⓒData Visualization Lab</div>
+        <div>Sejong University, Seoul, South Korea</div>
+        <div>contact: usangbong@gmail.com</div>
+      </>
+    );
+  }
+}
+
+export default Footer;
