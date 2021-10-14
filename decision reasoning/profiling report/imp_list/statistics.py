c12174c86ee6d242d0e1c87d70132ab91aed70e0
hhyein <48901291+hhyein@users.noreply.github.com>
2021-10-14 18:26:08 +0900

    Update statistics.py

--- decision reasoning/profiling report/imp_list/statistics.py
+++ decision reasoning/profiling report/imp_list/statistics.py
@@ -1,18 +1,12 @@
 import pandas as pd
-from scipy import stats
 import numpy as np
-
-df = pd.read_csv("static/data/missing.csv")
-df.head()
-
-
-########## Statistic on data quality
+from scipy import stats
 
 
 ##### skewness
 # Compute the sample skewness of a data set
 # url: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.skew.html
-def custom_skewness(input) :
+def custom_skewness(df, input) :
     output = stats.skew(df.iloc[:,input], nan_policy='omit')
     return output
 
@@ -21,7 +15,7 @@ def custom_skewness(input) :
 # Compute the kurtosis of a dataset
 # url: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.kurtosis.html
 #stats.kurtosis(df['col1'], fisher=True)
-def custom_kurtosis(input) :
+def custom_kurtosis(df, input) :
     output = stats.kurtosis(df.iloc[:,input], nan_policy='omit')
     return output
 
@@ -29,7 +23,7 @@ def custom_kurtosis(input) :
 ##### Kolmogorov-Smirnov test (KS test) of one sample
 # Performs the one sample KS test for goodness of fit
 # url: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.kstest.html#scipy.stats.kstest
-def custom_kstest(input) :
+def custom_kstest(df, input) :
     output = stats.kstest(df.iloc[:,input], 'norm')
     return output
 
@@ -38,7 +32,7 @@ def custom_kstest(input) :
 # Compute the KS test on two samples
 # url: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.ks_2samp.html#scipy.stats.ks_2samp
 #stats.ks_2samp(df['col1'],df['col2'], mode='auto')
-def custom_ks2test(input1, input2, matrix, imputation) :
+def custom_ks2test(df, input1, input2, matrix, imputation) :
     output = stats.ks_2samp(matrix.iloc[:,input1], imputation.iloc[:,input2], mode='auto')    
     return output
 
@@ -47,6 +41,6 @@ def custom_ks2test(input1, input2, matrix, imputation) :
 # Calculate the entropy of a distribution for given probability values.
 # url: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.entropy.html
 #stats.entropy(df['col1'], base=2)
-def custom_entropy(input) :
+def custom_entropy(df, input) :
     output = stats.entropy(df.iloc[:,input], base=2)
     return output
