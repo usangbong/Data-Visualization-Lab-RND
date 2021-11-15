2bd498e7b8e46bbb457eed63f567b2f24da86c3e
usangbong <49114064+usangbong@users.noreply.github.com>
2021-11-15 12:14:05 +0900

    Add files via upload

---
+++ gaze_project/dataCollection/NHB_survey/templates/src/components/EndContent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const EndContent=()=>{
+  return (
+    <>
+      <div className="bodyContDiv">
+        <h2 className="center">설문 조사 참여에 감사합니다.</h2>
+        <h2 className="center">문의는 usangbong@gmail.com으로 주세요.</h2>
+        <h2 className="center">웹 브라우저를 종료하셔도 됩니다.</h2>
+        <h2 className="center">오프라인 시선 데이터 수집 조사에 참석을 바라시면 메일로 연락주세요.</h2>
+        <h2 className="center">오프라인 시선 데이터 수집 조사에 참여하시면 별도의 리워드를 더 증정해드립니다.</h2>
+      </div>
+    </>
+  );
+  
+}
+
+export default EndContent;
