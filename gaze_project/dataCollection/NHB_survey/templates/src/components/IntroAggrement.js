0d258f2521cadef8b2dd2d56fa41cb7865f6e4ea
usangbong <49114064+usangbong@users.noreply.github.com>
2021-11-15 12:14:44 +0900

    Add files via upload

---
+++ gaze_project/dataCollection/NHB_survey/templates/src/components/IntroAggrement.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IntroAggrement=()=>{
+  return (
+    <>
+      <h2 className="center">개인정보의 수집 및 이용 동의서</h2>
+      <h4>이용자가 제공한 모든 정보는 다음의 목적을 위해 활용하며, 하기 목적 이외의 용도로는 사용되지 않습니다.</h4>
+      <div className="h4RedTextDiv">이 설문조사 페이지는 PC 사용에 최적화되어 있습니다.</div>
+      <div className="h4RedTextDiv">파이어폭스(Firefox) 브라우저는 사용이 불가능합니다.</div>
+    </>
+  );
+}
+
+export default IntroAggrement;
