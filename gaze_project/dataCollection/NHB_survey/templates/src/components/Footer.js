6387a5e49a4cb9711ac4822edbec65e8ca4150da
usangbong <49114064+usangbong@users.noreply.github.com>
2021-11-15 12:14:20 +0900

    Add files via upload

---
+++ gaze_project/dataCollection/NHB_survey/templates/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer=()=>{
+  return (
+    <>
+      <div>Data Visualization Lab,</div>
+      <div>Sejong University, Seoul, South Korea</div>
+      <div>contact: usangbong@gmail.com</div>
+    </>
+  );
+}
+
+export default Footer;
