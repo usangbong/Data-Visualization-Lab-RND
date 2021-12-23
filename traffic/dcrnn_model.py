1797fe8f2bf721457a7b7d42f86d184c73dd6e66
Mo0nl19ht <mool2480@gmail.com>
2021-12-23 17:43:02 +0900

    change

--- traffic/dcrnn_model.py
+++ traffic/dcrnn_model.py
@@ -63,7 +63,7 @@ class DCRNNModel(object):
                 tf.reshape(self._labels[..., :output_dim], (batch_size, horizon,num_nodes * output_dim)), axis=1)
             labels.insert(0, GO_SYMBOL)
             print("라벨")
-            print(labels)
+
 
 
 
